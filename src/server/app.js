4e40715234c08aec0a3e01428b38b5e05a9ac7ff
r3dDoX <patrick@walther-online.ch>
2016-11-23 21:28:42 +0100

    add docker-compose file to develop with mongoDB

--- src/server/app.js
+++ src/server/app.js
@@ -5,4 +5,6 @@ app.use('/', express.static('dist/'));
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port ${server.address().port}`);
+  console.log(`Database URI is: ${process.env.MONGODB_URI}`);
 });
+

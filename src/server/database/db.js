3eaafffbe5efa78d15473bc9f0b3fb18061e2148
r3dDoX <r3dDoX@users.noreply.github.com>
2016-11-26 12:16:43 +0100

    add flow checking and cloud foundry mongo config

--- src/server/database/db.js
+++ src/server/database/db.js
@@ -1,15 +1,24 @@
+/* @flow */
+
+const env = process.env;
+const mongoURI = env.MONGODB_URI || env.VCAP_SERVICES && env.VCAP_SERVICES.mongodb && env.VCAP_SERVICES.mongodb[0].uri;
 const MongoClient = require('mongodb').MongoClient;
 
 let db;
 
-MongoClient.connect(process.env.MONGODB_URI, (err, dbObject) => {
+MongoClient.connect(`${mongoURI}/geekplanet`, (err, dbObject) => {
   if (err) {
-    console.error(err);
+    console.dir(err);
   }
 
   db = dbObject;
 
   process.on('exit', () => db.close());
+
+  db.listCollections().toArray((err, items) => {
+    console.log(items);
+  });
+
 });
 
 module.exports = {

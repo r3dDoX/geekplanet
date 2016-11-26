e83794c63b24ab92ffdc8377619ca9632ed01fef
r3dDoX <patrick@walther-online.ch>
2016-11-26 12:16:43 +0100

    add flow checking and cloud foundry mongo config

--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,11 @@
+/* @flow */
+
 const express = require('express');
 const app = express();
 
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port ${server.address().port}`);
-  console.log(`Database URI is: ${process.env.MONGODB_URI}`);
 });
 
 app.use('/', express.static('dist/'));

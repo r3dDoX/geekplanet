331374bf806e2fee250ca7873e38e35381bd4291
r3dDoX <patrick@walther-online.ch>
2016-11-30 22:03:09 +0100

    use mongoose for better model handling

--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,39 @@
 /* @flow */
 
+const env = process.env;
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const db = require('./database/db');
+const mongoose = require('mongoose');
+
+const vcap_services = env.VCAP_SERVICES && JSON.parse(env.VCAP_SERVICES);
+const mongoURI = env.MONGODB_URI || vcap_services.mongodb && vcap_services.mongodb[0].credentials.database_uri;
+mongoose.connect(mongoURI);
+
+const AddressSchema = mongoose.Schema({
+  streetName: String,
+  streetNumber: String,
+  zip: Number,
+  city: String
+});
+
+const Product = mongoose.model('Prodcut', {
+  name: String,
+  price: Number,
+  stock: Number
+});
+
+const Producer = mongoose.model('Producer', {
+  name: String,
+  address: AddressSchema
+});
+
+const Supplier = mongoose.model('Supplier', {
+  name: String,
+  contact: String,
+  paymentTerms: String,
+  address: AddressSchema
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port ${server.address().port}`);
@@ -17,19 +47,32 @@ app.get('/api/test', (req, res) => {
 });
 
 app.post('/api/products', (req, res) => {
-  db.insertOrUpdate('products', req.body)
+  new Product(req.body).save()
     .then(() => res.sendStatus(200))
     .catch((error) => res.send(error));
 });
 
+const parseAddress = ({streetName, houseNumber, zip, city}) => ({
+  streetName,
+  houseNumber,
+  zip,
+  city
+});
+
 app.post('/api/suppliers', (req, res) => {
-  db.insertOrUpdate('suppliers', req.body)
+  const supplier = req.body;
+  supplier.address = parseAddress(supplier);
+
+  new Supplier(supplier).save()
     .then(() => res.sendStatus(200))
     .catch((error) => res.send(error));
 });
 
 app.post('/api/producers', (req, res) => {
-  db.insertOrUpdate('producers', req.body)
+  const producer = req.body;
+  producer.address = parseAddress(producer);
+
+  new Producer(producer).save()
     .then(() => res.sendStatus(200))
     .catch((error) => res.send(error));
 });

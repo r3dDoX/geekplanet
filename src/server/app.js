2db5dbbba5168ebe3bd5d4e2a2443ffd4f89ff89
r3dDoX <r3dDoX@users.noreply.github.com>
2016-12-03 12:57:33 +0100

    add flow-typed

--- src/server/app.js
+++ src/server/app.js
@@ -2,78 +2,13 @@
 
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cloudFoundryConfig = require('./cloudfoundry.config');
-
-const mongoURI = process.env.MONGODB_URI || cloudFoundryConfig.getMongoDbUri();
-mongoose.Promise = Promise;
-mongoose.connect(mongoURI);
-
-const AddressSchema = mongoose.Schema({
-  streetName: String,
-  streetNumber: String,
-  zip: Number,
-  city: String
-});
-
-const Product = mongoose.model('Product', {
-  name: String,
-  price: Number,
-  stock: Number
-});
-
-const Producer = mongoose.model('Producer', {
-  name: String,
-  address: AddressSchema
-});
-
-const Supplier = mongoose.model('Supplier', {
-  name: String,
-  contact: String,
-  paymentTerms: String,
-  address: AddressSchema
-});
+const api = require('./api');
 
 const server = app.listen(process.env.PORT || 3000, () => {
-  console.log(`Listening on port ${server.address().port}`);
+  console.info(`Listening on port ${server.address().port}`);
 });
 
-app.use(bodyParser.json());
 app.use('/', express.static('dist/'));
 app.get('/*', (req, res) => res.sendfile('dist/index.html'));
 
-app.get('/api/test', (req, res) => {
-  res.send('Hello World');
-});
-
-app.post('/api/products', (req, res) => {
-  new Product(req.body).save()
-    .then(() => res.sendStatus(200))
-    .catch((error) => res.send(error));
-});
-
-const parseAddress = ({streetName, houseNumber, zip, city}) => ({
-  streetName,
-  houseNumber,
-  zip,
-  city
-});
-
-app.post('/api/suppliers', (req, res) => {
-  const supplier = req.body;
-  supplier.address = parseAddress(supplier);
-
-  new Supplier(supplier).save()
-    .then(() => res.sendStatus(200))
-    .catch((error) => res.send(error));
-});
-
-app.post('/api/producers', (req, res) => {
-  const producer = req.body;
-  producer.address = parseAddress(producer);
-
-  new Producer(producer).save()
-    .then(() => res.sendStatus(200))
-    .catch((error) => res.send(error));
-});
+api.registerEndpoints(app);

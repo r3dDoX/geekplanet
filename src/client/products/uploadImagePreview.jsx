708fe3780c94535e7322a5d597c1c48f0c60178a
r3dDoX <patrick@walther-online.ch>
2016-12-10 19:08:59 +0100

    add nice gridlist view

--- src/client/products/uploadImagePreview.jsx
+++ src/client/products/uploadImagePreview.jsx
@@ -1,8 +1,24 @@
 import React, { PropTypes } from 'react';
+import { GridList, GridTile } from 'material-ui/GridList';
+
+const containerStyle = {
+  marginTop: '20px',
+};
 
 const UploadImagePreview = props => (
-  <div>
-    {(props.files) ? Array.from(props.files).map((file, index) => <img key={index} alt="Preview" height="100px" src={URL.createObjectURL(file)} />) : ''}
+  <div style={ containerStyle }>
+    { props.files ? (
+      <GridList cols={3}>
+        {Array.from(props.files).map((file, index) => (
+          <GridTile
+            key={index}
+            title={file.name}
+          >
+            <img alt={file.name} src={URL.createObjectURL(file)}/>
+          </GridTile>
+        ))}
+      </GridList>
+    ) : null}
   </div>
 );
 

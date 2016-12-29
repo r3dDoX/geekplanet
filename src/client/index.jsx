b3bf033f61e8a3966619bd1ee64b294c65233222
r3dDoX <r3dDoX@users.noreply.github.com>
2016-12-29 17:17:14 +0100

    introduce translations

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,33 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { IntlProvider, addLocaleData } from 'react-intl';
-import en from 'react-intl/locale-data/en';
-import de from 'react-intl/locale-data/de';
-import fr from 'react-intl/locale-data/fr';
-import it from 'react-intl/locale-data/it';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './app.jsx';
-import reducers from './reducers';
 import geekplanetTheme from './theme';
-
-addLocaleData([...de, ...en, ...fr, ...it]);
-const language = (navigator.languages && navigator.languages[0])
-  || navigator.language
-  || navigator.userLanguage;
+import reducers from './reducers';
 
 injectTapEventPlugin();
-
 const store = createStore(reducers);
 
 ReactDOM.render((
-  <Provider store={store}>
-    <IntlProvider locale={language}>
-      <MuiThemeProvider muiTheme={geekplanetTheme}>
-        <App store={store} />
-      </MuiThemeProvider>
-    </IntlProvider>
-  </Provider>
+  <MuiThemeProvider muiTheme={geekplanetTheme}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </MuiThemeProvider>
 ), document.getElementsByTagName('main')[0]);

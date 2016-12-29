e66f66086b6cd2b770f30ae069ef99b4450bea0c
r3dDoX <patrick@walther-online.ch>
2016-12-29 01:19:43 +0100

    prepare app for i18n

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import React, { PropTypes } from 'react';
 import Router from 'react-router/lib/Router';
 import Route from 'react-router/lib/Route';
 import IndexRoute from 'react-router/lib/IndexRoute';
 import browserHistory from 'react-router/lib/browserHistory';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 import { syncHistoryWithStore } from 'react-router-redux';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import Home from './home/home.jsx';
 import Layout from './layout.jsx';
 import FormsContainer from './forms/formsContainer';
-import reducers from './reducers';
-import './app.less';
 
-injectTapEventPlugin();
+class App extends React.Component {
 
-const store = createStore(reducers);
+  componentWillMount() {
+    // TODO load translations
+  }
 
-const history = syncHistoryWithStore(browserHistory, store);
+  render() {
+    const history = syncHistoryWithStore(browserHistory, this.props.store);
 
-ReactDOM.render((
-  <Provider store={store}>
-    <MuiThemeProvider>
+    return (
       <Router history={history}>
         <Route path="/" component={Layout}>
           <IndexRoute component={Home} />
           <Route path="forms" component={FormsContainer} />
         </Route>
       </Router>
-    </MuiThemeProvider>
-  </Provider>
-), document.getElementsByTagName('main')[0]);
+    );
+  }
+}
+
+App.propTypes = {
+  store: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+};
+
+export default App;
---
+++ src/client/theme.js
@@ -0,0 +1,7 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+export default getMuiTheme({
+  palette: {
+    primary1Color: '#27AAE1',
+  },
+});

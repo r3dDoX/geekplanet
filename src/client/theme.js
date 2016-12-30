bc1faee49c34724704e5201aef17222f137edf96
r3dDoX <r3dDoX@users.noreply.github.com>
2016-12-30 17:24:02 +0100

    add some more styling

--- src/client/theme.js
+++ src/client/theme.js
@@ -1,7 +1,8 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 export default getMuiTheme({
+  fontFamily: 'PT Sans, sans-serif',
   palette: {
-    primary1Color: '#27AAE1',
+    primary1Color: '#131E31',
   },
 });

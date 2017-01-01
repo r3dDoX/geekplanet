d6217fc5be6a2b974f4d9bb45e966102af12d137
r3dDoX <patrick@walther-online.ch>
2017-01-01 21:52:57 +0100

    add auth0 for authentication

--- src/client/layout.jsx
+++ src/client/layout.jsx
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
 import Link from 'react-router/lib/Link';
+import * as AuthService from './auth/authService';
 
 const styles = {
   container: {
@@ -17,23 +18,27 @@ const styles = {
   },
 };
 
-const Layout = props => (
-  <div style={styles.container}>
-    <AppBar
-      title={<Link to="/" style={styles.title}>Geekplanet</Link>}
-      showMenuIconButton={false}
-      iconElementRight={
-        <FlatButton label="Forms" containerElement={<Link to="/forms">Forms</Link>} />
-      }
-      style={styles.appBar}
-      zDepth={0}
-    />
-    {props.children}
-  </div>
-);
+const Layout = ({ auth, children }) => {
+  return (
+    <div style={styles.container}>
+      <AppBar
+        title={<Link to="/" style={styles.title}>Geekplanet</Link>}
+        showMenuIconButton={false}
+        iconElementRight={auth.loggedIn() ?
+          <FlatButton label="Forms" containerElement={<Link to="/forms">Forms</Link>} /> :
+          <FlatButton label="Login" primary onClick={() => auth.login()} />
+        }
+        style={styles.appBar}
+        zDepth={0}
+      />
+      {children}
+    </div>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.element.isRequired,
+  auth: PropTypes.shape(AuthService.Prototype),
 };
 
 export default Layout;

1ba7f1d7a3d8f2caf5d95c1e88f2083e88234307
r3dDoX <r3dDoX@users.noreply.github.com>
2017-01-01 23:23:29 +0100

    add env specific configs

--- src/client/auth/authService.js
+++ src/client/auth/authService.js
@@ -30,7 +30,7 @@ export default {
 
     obj.lock = new Auth0Lock(clientId, domain, {
       auth: {
-        redirectUrl: 'http://localhost:3001/', // TODO this url has to be configurable
+        redirectUrl: AUTH.REDIRECT_URL,
         responseType: 'token',
       },
     });

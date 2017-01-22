5b2087f8e99d13231b7e751b7c0189378b54cf94
r3dDoX <patrick@walther-online.ch>
2017-01-22 17:57:47 +0100

    revert deleted styles

--- src/client/admin/forms/productForm.jsx
+++ src/client/admin/forms/productForm.jsx
@@ -16,6 +16,19 @@ const styles = {
   container: {
     padding: '20px',
   },
+  fileUploadInput: {
+    cursor: 'pointer',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    width: '100%',
+    opacity: 0,
+  },
+  uploadButton: {
+    marginTop: '10px',
+  },
 };
 
 const ProductForm = ({

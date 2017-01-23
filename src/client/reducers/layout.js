c1781ae13e96fc9b876abec4451c08305d168506
r3dDoX <patrick@walther-online.ch>
2017-01-23 22:55:09 +0100

    show snackbar when item added

--- src/client/reducers/layout.js
+++ src/client/reducers/layout.js
@@ -2,6 +2,7 @@ import ActionTypes from '../actionTypes';
 
 const initialState = {
   drawerOpened: false,
+  shoppingCartNotification: false,
 };
 
 export default function auth(state = initialState, { type }) {
@@ -9,6 +10,14 @@ export default function auth(state = initialState, { type }) {
     case ActionTypes.TOGGLE_DRAWER:
       return Object.assign({}, state, {
         drawerOpened: !state.drawerOpened,
+      });
+    case ActionTypes.ADD_ITEM_TO_SHOPPING_CART:
+      return Object.assign({}, state, {
+        shoppingCartNotification: true,
+      });
+    case ActionTypes.HIDE_SHOPPING_CART_NOTIFICATION:
+      return Object.assign({}, state, {
+        shoppingCartNotification: false,
       });
     default:
       return state;

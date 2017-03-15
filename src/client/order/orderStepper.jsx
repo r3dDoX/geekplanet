cc0a92874f61db46334b210beae56bb08c20813f
r3dDoX <r3dDoX@users.noreply.github.com>
2017-03-15 22:57:55 +0100

    implement stripe checkout

--- src/client/order/orderStepper.jsx
+++ src/client/order/orderStepper.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
 import {
   Step,
   Stepper,
@@ -6,8 +7,9 @@ import {
   StepContent,
 } from 'material-ui/Stepper';
 import { FormattedMessage } from 'react-intl';
-import { DatatransProvider } from 'react-datatrans-lightbox';
-import LightboxOpener from './lightboxOpener.jsx';
+import RaisedButton from 'material-ui/RaisedButton';
+import Xhr from '../xhr';
+import { ShoppingCartPropType } from '../shoppingcart/shoppingCart.proptypes';
 
 const styles = {
   container: {
@@ -39,31 +41,85 @@ const styles = {
   },
 };
 
-export default () => (
-  <div style={styles.container}>
-    <Stepper
-      activeStep={0}
-      linear={false}
-      orientation="vertical"
-    >
-      <Step>
-        <StepButton>
-          <FormattedMessage id="ORDER.PAYMENT.TITLE" />
-        </StepButton>
-        <StepContent>
-          <DatatransProvider endpoint="https://pilot.datatrans.biz">
-            <LightboxOpener />
-          </DatatransProvider>
-        </StepContent>
-      </Step>
-      <Step>
-        <StepButton>
-          <FormattedMessage id="ORDER.ADDRESS.TITLE" />
-        </StepButton>
-        <StepContent>
-          <p>An ad group contains one or more ads which target a shared set of keywords.</p>
-        </StepContent>
-      </Step>
-    </Stepper>
-  </div>
+const startOrder = shoppingCart => Xhr.post(
+  '/api/orders',
+  JSON.stringify(shoppingCart),
+  'application/json'
 );
+const removeOrder = shoppingCartId => Xhr.deleteHttp(`/api/orders/${shoppingCartId}`);
+
+const OrderStepper = ({ email, shoppingCart }) => {
+  const stripeConfig = {
+    key: PAYMENT_PUBLIC,
+    locale: 'auto',
+    token: token => Xhr.post(
+      '/api/payment',
+      JSON.stringify({ token, shoppingCartId: shoppingCart.id }),
+      'application/json'
+    ),
+    opened: () => startOrder(shoppingCart),
+    closed: () => removeOrder(shoppingCart.id),
+  };
+
+  if (email) {
+    stripeConfig.email = email;
+  }
+
+  const handler = StripeCheckout.configure(stripeConfig);
+
+  return (
+    <div style={styles.container}>
+      <Stepper
+        activeStep={0}
+        linear={false}
+        orientation="vertical"
+      >
+        <Step>
+          <StepButton>
+            <FormattedMessage id="ORDER.PAYMENT.TITLE" />
+          </StepButton>
+          <StepContent>
+            <RaisedButton
+              onClick={() => handler.open({
+                name: 'Geekplanet GmbH',
+                image: '/assets/images/icon.png',
+                currency: 'chf',
+                amount: shoppingCart.items.reduce(
+                  (sum, { amount, product }) => (amount * product.price * 100) + sum,
+                  0
+                ),
+              })}
+              label={<FormattedMessage id="ORDER.PAYMENT.SELECT_PAYMENT_METHOD" />}
+              primary
+            />
+          </StepContent>
+        </Step>
+        <Step>
+          <StepButton>
+            <FormattedMessage id="ORDER.ADDRESS.TITLE" />
+          </StepButton>
+          <StepContent>
+            <p>An ad group contains one or more ads which target a shared set of keywords.</p>
+          </StepContent>
+        </Step>
+      </Stepper>
+    </div>
+  );
+};
+
+OrderStepper.defaultProps = {
+  email: '',
+};
+
+OrderStepper.propTypes = {
+  email: PropTypes.string,
+  shoppingCart: ShoppingCartPropType.isRequired,
+};
+
+export default connect(
+  state => ({
+    email: state.auth.email,
+    shoppingCart: state.shoppingCart,
+  }),
+  () => ({})
+)(OrderStepper);

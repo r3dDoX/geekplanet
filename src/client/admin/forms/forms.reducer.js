90d5d8a5f4cfd3fe260f3cd33ce76d0749aba1ab
r3dDoX <r3dDoX@users.noreply.github.com>
2017-03-16 21:39:54 +0100

    move reducers to features

--- src/client/admin/forms/forms.reducer.js
+++ src/client/admin/forms/forms.reducer.js
@@ -1,4 +1,4 @@
-import ActionTypes from '../actionTypes';
+import ActionTypes from '../../actionTypes';
 
 const initialState = {
   selectedFiles: undefined,
--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
-import products from './products';
-import forms from './forms';
-import i18n from './i18n';
-import auth from './auth';
-import layout from './layout';
-import shoppingCart from './shoppingCart';
+import products from '../products/products.reducer';
+import forms from '../admin/forms/forms.reducer';
+import i18n from '../i18n/i18n.reducer';
+import auth from '../auth/auth.reducer';
+import layout from '../layout/layout.reducer';
+import shoppingCart from '../shoppingcart/shoppingCart.reducer';
 
 export default combineReducers({
   products,

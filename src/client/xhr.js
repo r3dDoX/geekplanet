2b039498b1120c69e42958d601dc2284c341e805
r3dDoX <patrick@walther-online.ch>
2017-03-19 23:32:16 +0100

    refactor forms to use redux-form

--- src/client/xhr.js
+++ src/client/xhr.js
@@ -1,76 +1,55 @@
 import * as storage from './storage';
 
-const setAuthHeader = (req) => {
+function setAuthHeader(req) {
   const jwtToken = storage.load(storage.ids.ID_TOKEN);
   if (jwtToken) {
     req.setRequestHeader('Authorization', `Bearer ${jwtToken}`);
   }
-};
+}
+
+function wrapXhrPromise(path, method, data, contentType) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('load', () => {
+      if (request.status === 200) {
+        let response = request.response;
+        try {
+          response = JSON.parse(response);
+        } catch (e) { /* Nothing to do when no JSON returned*/ }
+        resolve(response);
+      } else {
+        reject();
+      }
+    });
+
+    request.addEventListener('error', () => {
+      reject();
+    });
+
+    request.open(method, path);
+    setAuthHeader(request);
+    if (contentType) {
+      request.setRequestHeader('Content-Type', contentType);
+    }
+    request.send(JSON.stringify(data));
+  });
+}
 
 export default {
   get(path) {
-    return new Promise((resolve, reject) => {
-      const request = new XMLHttpRequest();
-
-      request.addEventListener('load', () => {
-        if (request.status === 200) {
-          resolve(JSON.parse(request.response));
-        } else {
-          reject();
-        }
-      });
-
-      request.open('GET', path);
-      setAuthHeader(request);
-      request.send();
-    });
+    return wrapXhrPromise(path, 'GET');
   },
 
   post(path, data, contentType) {
-    return new Promise((resolve, reject) => {
-      const request = new XMLHttpRequest();
+    return wrapXhrPromise(path, 'POST', data, contentType);
+  },
 
-      request.addEventListener('load', () => {
-        if (request.status === 200) {
-          resolve(request.response);
-        } else {
-          reject();
-        }
-      });
-
-      request.addEventListener('error', () => {
-        reject();
-      });
-
-      request.open('POST', path);
-      setAuthHeader(request);
-      if (contentType) {
-        request.setRequestHeader('Content-Type', contentType);
-      }
-      request.send(data);
-    });
+  put(path, data, contentType) {
+    return wrapXhrPromise(path, 'PUT', data, contentType);
   },
 
   deleteHttp(path) {
-    return new Promise((resolve, reject) => {
-      const request = new XMLHttpRequest();
-
-
-      request.addEventListener('load', () => {
-        if (request.status === 200) {
-          resolve();
-        } else {
-          reject();
-        }
-      });
-
-      request.addEventListener('error', () => {
-        reject();
-      });
-
-      request.open('DELETE', path);
-      setAuthHeader(request);
-      request.send();
-    });
+    return wrapXhrPromise(path, 'DELETE');
   },
 };

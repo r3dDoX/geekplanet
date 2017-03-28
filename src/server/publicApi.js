85279cae7c5a3094af04d7bbe332881a4d5ee6e9
r3dDoX <r3dDoX@users.noreply.github.com>
2017-03-28 22:57:20 +0200

    sort categories by name upon retrieval

--- src/server/publicApi.js
+++ src/server/publicApi.js
@@ -21,7 +21,13 @@ module.exports = {
     });
 
     app.get('/api/productcategories', (req, res) => ProductCategory.find({})
-      .then(categories => res.send(categories))
+      .then(categories => res.send(categories.sort((a, b) => {
+        const nameA = a.name.toUpperCase();
+        const nameB = b.name.toUpperCase();
+        if (nameA < nameB) return -1;
+        if (nameA > nameB) return 1;
+        return 0;
+      })))
       .catch((err) => {
         console.error(err);
         res.status(500).send('Fetching product categories failed!');

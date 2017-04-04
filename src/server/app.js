a706da8fbcf26a14c685961a7aa893bb6933f1e4
r3dDoX <r3dDoX@users.noreply.github.com>
2017-04-04 16:51:06 +0200

    add some proper flow typings

--- src/server/app.js
+++ src/server/app.js
@@ -8,6 +8,13 @@ const mongoSanitize = require('express-mongo-sanitize');
 const cloudFoundryConfig = require('./cloudfoundry.config.js');
 
 const app = express();
+app.use('*', (req /* : express$Request */, res /* : express$Response */, next /* : express$NextFunction */) => {
+  if (process.env.NODE_ENV === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
+    res.redirect(`https://${req.hostname}${req.url}`);
+  } else {
+    next();
+  }
+});
 app.use(compression());
 app.use(mongoSanitize());
 
@@ -19,14 +26,6 @@ const server = app.listen(process.env.PORT || 3000, () => {
   console.info(`Listening on port ${server.address().port}`);
 });
 
-app.use('*', (req, res, next) => {
-  if (process.env.NODE_ENV === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
-    res.redirect(`https://${req.host}${req.url}`);
-  } else {
-    next();
-  }
-});
-
 app.use('/', express.static('dist/', {
   maxAge: '1d',
 }));
@@ -34,4 +33,6 @@ app.use('/', express.static('dist/', {
 require('./authorizedApi').registerEndpoints(app);
 require('./publicApi').registerEndpoints(app);
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '../../dist', 'index.html')));
+app.get('/*', (req /* : express$Request */, res /* : express$Response */) =>
+  res.sendFile(path.join(__dirname, '../../dist', 'index.html'))
+);
--- src/server/authorizedApi.js
+++ src/server/authorizedApi.js
@@ -26,7 +26,7 @@ const authorization = jwt({
   audience: process.env.AUDIENCE || secretConfig.AUDIENCE,
 });
 
-function isAdmin(req, res, next) {
+function isAdmin(req /* : express$Request */, res /* : express$Response */, next /* : express$NextFunction */) {
   if (!req.user || req.user.app_metadata.roles.indexOf('admin') === -1) {
     res.sendStatus(403);
     return;
@@ -47,126 +47,137 @@ function saveOrUpdate(Collection, document) {
 }
 
 module.exports = {
-  registerEndpoints(app) {
-    app.post('/api/products/pictures', authorization, isAdmin, multer.any(), (req, res) =>
-      Promise.all(req.files.map(file =>
-        new Promise((resolve, reject) =>
-          ProductPictures.write(
-            {
-              filename: `${shortId.generate()}.${file.originalname.split(/[. ]+/).pop()}`,
-              contentType: file.mimetype,
-            },
-            streamifier.createReadStream(file.buffer),
-            (error, createdFile) => (error ? reject(error) : resolve(createdFile._id))
+  registerEndpoints(app /* : express$Application */) {
+    app.post('/api/products/pictures', authorization, isAdmin, multer.any(),
+      (req /* : express$Request */, res /* : express$Response */) =>
+        Promise.all(req.files.map(file =>
+          new Promise((resolve, reject) =>
+            ProductPictures.write(
+              {
+                filename: `${shortId.generate()}.${file.originalname.split(/[. ]+/).pop()}`,
+                contentType: file.mimetype,
+              },
+              streamifier.createReadStream(file.buffer),
+              (error, createdFile) => (error ? reject(error) : resolve(createdFile._id))
+            )
           )
-        )
-      ))
-        .then(files => res.status(200).send(JSON.stringify(files)))
-        .catch(error => handleGenericError(error, res))
+        ))
+          .then(files => res.status(200).send(JSON.stringify(files)))
+          .catch(error => handleGenericError(error, res))
     );
 
-    app.delete('/api/products/pictures/:id', authorization, isAdmin, (req, res) =>
-      ProductPictures.remove({ _id: req.params.id })
-        .then(() => Product.update({}, { $pull: { files: req.params.id } }, { multi: true }))
-        .then(() => res.sendStatus(200))
-        .catch(error => handleGenericError(error, res))
+    app.delete('/api/products/pictures/:id', authorization, isAdmin,
+      (req /* : express$Request */, res /* : express$Response */) =>
+        ProductPictures.remove({ _id: req.params.id })
+          .then(() => Product.update({}, { $pull: { files: req.params.id } }, { multi: true }))
+          .then(() => res.sendStatus(200))
+          .catch(error => handleGenericError(error, res))
     );
 
-    app.put('/api/products', authorization, isAdmin, bodyParser.json(), (req, res) =>
-      saveOrUpdate(Product, req.body)
-        .then(() => res.sendStatus(200))
-        .catch(handleGenericError)
+    app.put('/api/products', authorization, isAdmin, bodyParser.json(),
+      (req /* : express$Request */, res /* : express$Response */) =>
+        saveOrUpdate(Product, req.body)
+          .then(() => res.sendStatus(200))
+          .catch(handleGenericError)
     );
 
-    app.put('/api/tags', authorization, isAdmin, bodyParser.json(), (req, res) =>
-      saveOrUpdate(Tag, req.body)
-        .then(() => res.sendStatus(200))
-        .catch(handleGenericError)
+    app.put('/api/tags', authorization, isAdmin, bodyParser.json(),
+      (req /* : express$Request */, res /* : express$Response */) =>
+        saveOrUpdate(Tag, req.body)
+          .then(() => res.sendStatus(200))
+          .catch(handleGenericError)
     );
 
-    app.put('/api/productcategories', authorization, isAdmin, bodyParser.json(), ({ body }, res) => {
-      const promises = [];
-      if (body._id) {
-        ProductCategory.findOne({ _id: body._id })
-          .then(({ name }) => {
-            if (name !== body.name) {
-              promises.push(ProductCategory.update(
-                { parentCategory: name },
-                { $set: { parentCategory: body.name } },
-                { multi: true }).exec());
-            }
-          });
-      }
-      promises.push(saveOrUpdate(ProductCategory, body));
-      Promise.all(promises)
-        .then(() => res.sendStatus(200))
-        .catch(error => handleGenericError(error, res));
-    });
-
-    app.put('/api/suppliers', authorization, isAdmin, bodyParser.json(), (req, res) =>
-      saveOrUpdate(Supplier, req.body)
-        .then(() => res.sendStatus(200))
-        .catch(error => handleGenericError(error, res))
-    );
-
-    app.put('/api/producers', authorization, isAdmin, bodyParser.json(), (req, res) =>
-      saveOrUpdate(Producer, req.body)
-        .then(() => res.sendStatus(200))
-        .catch(error => handleGenericError(error, res))
-    );
-
-    app.put('/api/orders', authorization, bodyParser.json(), (req, res) =>
-      saveOrUpdate(Order, Object.assign(
-        req.body,
-        {
-          user: req.user.user_id,
-          state: OrderState.STARTED,
-          date: Date.now(),
+    app.put('/api/productcategories', authorization, isAdmin, bodyParser.json(),
+      ({ body } /* : express$Request */, res /* : express$Response */) => {
+        const promises = [];
+        if (body._id) {
+          ProductCategory.findOne({ _id: body._id })
+            .then(({ name }) => {
+              if (name !== body.name) {
+                promises.push(ProductCategory.update(
+                  { parentCategory: name },
+                  { $set: { parentCategory: body.name } },
+                  { multi: true }).exec());
+              }
+            });
         }
-      ))
-        .then(() => res.sendStatus(200))
-        .catch(error => handleGenericError(error, res))
+        promises.push(saveOrUpdate(ProductCategory, body));
+        Promise.all(promises)
+          .then(() => res.sendStatus(200))
+          .catch(error => handleGenericError(error, res));
+      });
+
+    app.put('/api/suppliers', authorization, isAdmin, bodyParser.json(),
+      (req /* : express$Request */, res /* : express$Response */) =>
+        saveOrUpdate(Supplier, req.body)
+          .then(() => res.sendStatus(200))
+          .catch(error => handleGenericError(error, res))
     );
 
-    app.put('/api/userAddress', authorization, bodyParser.json(), (req, res) =>
-      saveOrUpdate(UserAddress, Object.assign(
-        req.body,
-        { user: req.user.user_id }
-      ))
-        .then(address => res.status(200).send(address._id))
-        .catch(error => handleGenericError(error, res))
+    app.put('/api/producers', authorization, isAdmin, bodyParser.json(),
+      (req /* : express$Request */, res /* : express$Response */) =>
+        saveOrUpdate(Producer, req.body)
+          .then(() => res.sendStatus(200))
+          .catch(error => handleGenericError(error, res))
     );
 
-    app.get('/api/userAddresses', authorization, (req, res) =>
-      UserAddress.find({ user: req.user.user_id }, { user: 0 })
-        .then(addresses => res.status(200).send(JSON.stringify(addresses)))
-        .catch(error => handleGenericError(error, res))
+    app.put('/api/orders', authorization, bodyParser.json(),
+      (req /* : express$Request */, res /* : express$Response */) =>
+        saveOrUpdate(Order, Object.assign(
+          req.body,
+          {
+            user: req.user.user_id,
+            state: OrderState.STARTED,
+            date: Date.now(),
+          }
+        ))
+          .then(() => res.sendStatus(200))
+          .catch(error => handleGenericError(error, res))
     );
 
-    app.post('/api/payment', bodyParser.json(), authorization, (req, res) => {
-      const orderQuery = Order.findOne({ _id: req.body.shoppingCartId, user: req.user.user_id });
-      let items;
+    app.put('/api/userAddress', authorization, bodyParser.json(),
+      (req /* : express$Request */, res /* : express$Response */) =>
+        saveOrUpdate(UserAddress, Object.assign(
+          req.body,
+          { user: req.user.user_id }
+        ))
+          .then(address => res.status(200).send(address._id))
+          .catch(error => handleGenericError(error, res))
+    );
 
-      orderQuery
-        .then((order) => {
-          items = order.items;
-          return stripe.charges.create({
-            amount: order.items.reduce(
-              (sum, { amount, product }) => sum + (amount * product.price * 100), 0
-            ),
-            description: order._id,
-            currency: 'chf',
-            source: req.body.token.id,
-          });
-        })
-        .then(() => {
-          orderQuery.update({ state: OrderState.FINISHED });
-          items.forEach(({ amount, product }) =>
-            Product.update({ _id: product._id }, { $inc: { stock: amount * -1 } }).exec()
-          );
-        })
-        .then(() => res.sendStatus(200))
-        .catch(error => handleGenericError(error, res));
-    });
+    app.get('/api/userAddresses', authorization,
+      (req /* : express$Request */, res /* : express$Response */) =>
+        UserAddress.find({ user: req.user.user_id }, { user: 0 })
+          .then(addresses => res.status(200).send(JSON.stringify(addresses)))
+          .catch(error => handleGenericError(error, res))
+    );
+
+    app.post('/api/payment', bodyParser.json(), authorization,
+      (req /* : express$Request */, res /* : express$Response */) => {
+        const orderQuery = Order.findOne({ _id: req.body.shoppingCartId, user: req.user.user_id });
+        let items;
+
+        orderQuery
+          .then((order) => {
+            items = order.items;
+            return stripe.charges.create({
+              amount: order.items.reduce(
+                (sum, { amount, product }) => sum + (amount * product.price * 100), 0
+              ),
+              description: order._id,
+              currency: 'chf',
+              source: req.body.token.id,
+            });
+          })
+          .then(() => {
+            orderQuery.update({ state: OrderState.FINISHED });
+            items.forEach(({ amount, product }) =>
+              Product.update({ _id: product._id }, { $inc: { stock: amount * -1 } }).exec()
+            );
+          })
+          .then(() => res.sendStatus(200))
+          .catch(error => handleGenericError(error, res));
+      });
   },
 };

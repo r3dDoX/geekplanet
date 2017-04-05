afff5cbb6457ca24e654e0405d2d3b53f9de6dc9
r3dDoX <r3dDoX@users.noreply.github.com>
2017-04-05 22:42:53 +0200

    calculate values for esr

--- src/server/esr/esrGenerator.js
+++ src/server/esr/esrGenerator.js
@@ -5,17 +5,26 @@ const pdfFiller = require('pdffiller');
 const esrTemplate = `${__dirname}/ESR_Form.pdf`;
 
 module.exports = {
-  generate() {
+  generate(
+    value /* : number */,
+    address /* : any */
+  ) {
     const destinationPDF = './invoices/test_complete.pdf';
+    const decimalValue = Math.round((value % 1) * 100);
+    const integerValue = Math.floor(value);
+    const esrAddress = `${address.firstName} ${address.lastName}
+${address.streetAddress}
+${address.zip} ${address.city}`;
+
     const formData = {
-      value_1: '400',
-      decimal_1: '00',
-      value_2: '400',
-      decimal_2: '00',
+      value_1: integerValue,
+      decimal_1: decimalValue,
+      value_2: integerValue,
+      decimal_2: decimalValue,
       reference: '96 11169 00000 00660 00000 09284',
       reference_small: '961116900000006600000009284',
-      address_1: 'UTF-8Täst\nTüststrasse 2B\n5000 Önsé',
-      address_2: 'UTF-8Täst\nTüststrasse 2B\n5000 Önsé',
+      address_1: esrAddress,
+      address_2: esrAddress,
       esr_code: '2300000440009>961116900000006600000009284+ 030001625>',
     };
 

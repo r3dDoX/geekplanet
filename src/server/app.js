934c90a03429706022b606a392cadaf63c01c0ff
r3dDoX <patrick@walther-online.ch>
2017-04-05 16:28:43 +0200

    also consider mongohq if set

--- src/server/app.js
+++ src/server/app.js
@@ -9,7 +9,11 @@ const cloudFoundryConfig = require('./cloudfoundry.config.js');
 const Logger = require('./logger');
 
 const app = express();
-app.use('*', (req /* : express$Request */, res /* : express$Response */, next /* : express$NextFunction */) => {
+app.use('*', (
+  req /* : express$Request */,
+  res /* : express$Response */,
+  next /* : express$NextFunction */
+) => {
   if (process.env.NODE_ENV === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
     res.redirect(`https://${req.hostname}${req.url}`);
   } else {
@@ -19,7 +23,9 @@ app.use('*', (req /* : express$Request */, res /* : express$Response */, next /*
 app.use(compression());
 app.use(mongoSanitize());
 
-const mongoURI = process.env.MONGODB_URI || cloudFoundryConfig.getMongoDbUri();
+const mongoURI = process.env.MONGODB_URI
+  || process.env.MONGOHQ_URI
+  || cloudFoundryConfig.getMongoDbUri();
 mongoose.Promise = Promise;
 mongoose.connect(mongoURI);
 

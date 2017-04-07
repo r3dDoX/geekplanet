b1c903e2337e5fb4d373ff57e6f7d7b4d0e14941
r3dDoX <patrick@walther-online.ch>
2017-04-07 09:19:26 +0200

    reduce bundle size of prod build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -5,18 +5,10 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common.config');
 
-let definePlugin;
 const secretConfig = require('./src/config/secret.config.json');
-
 Object.keys(secretConfig).map((key) => {
   secretConfig[key] = `'${secretConfig[key] || process.env[key]}'`;
 });
-
-if (process.env.NODE && ~process.env.NODE.indexOf('heroku')) {
-  definePlugin = new webpack.DefinePlugin(Object.assign(require('./src/config/heroku.config.json'), secretConfig));
-} else {
-  definePlugin = new webpack.DefinePlugin(Object.assign(require('./src/config/sccloud.config.json'), secretConfig));
-}
 
 module.exports = merge(commonConfig, {
   entry: './src/client/index.jsx',
@@ -27,6 +19,14 @@ module.exports = merge(commonConfig, {
   },
 
   plugins: [
-    definePlugin,
+    new webpack.DefinePlugin(Object.assign(
+      require('./src/config/heroku.config.json'),
+      secretConfig,
+      {
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production'),
+        },
+      }
+    )),
   ],
 });

8585e4fe0c704a0f9cac568a214aa19a5ea475b7
r3dDoX <patrick@walther-online.ch>
2017-04-10 09:46:42 +0200

    more tests

--- src/server/esr/esrCodeHelpers.test.js
+++ src/server/esr/esrCodeHelpers.test.js
@@ -1,5 +1,31 @@
 const underTest = require('./esrCodeHelpers');
 
+describe('generateValueCode', () => {
+  test('should generate 12 digit code out of price with checkdigit', () => {
+    const price = 498.25;
+
+    const result = underTest.generateValueCode(price);
+
+    expect(result).toBe('0100000498259');
+  });
+
+  test('should pad price with zeros when no decimal', () => {
+    const price = 123;
+
+    const result = underTest.generateValueCode(price);
+
+    expect(result).toBe('0100000123009');
+  });
+});
+
+describe('generateInvoiceNumberCode', () => {
+  test('should pad invoice number with zeros and calculate checkdigit', () => {
+    const result = underTest.generateInvoiceNumberCode(524);
+
+    expect(result).toBe('000005039981000000000005247');
+  });
+});
+
 describe('spaceReference', () => {
   test('should add a space every 5 chars from right', () => {
     const result = underTest.spaceReference('000005039981000000000000018');

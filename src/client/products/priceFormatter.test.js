d5eb8c22ac877c832aa6114886b0d0f6fdeb697e
r3dDoX <r3dDoX@users.noreply.github.com>
2017-04-13 17:41:06 +0200

    add test for price formatter

---
+++ src/client/products/priceFormatter.test.js
@@ -0,0 +1,29 @@
+const underTest = require('./priceFormatter');
+
+describe('formatPriceWithoutCurrency', () => {
+  test('should format price with two decimals in steps of five', () => {
+    const price = 123.456;
+
+    const result = underTest.formatPriceWithoutCurrency(price);
+
+    expect(result).toBe('123.45');
+  });
+
+  test('should format price with dash when round', () => {
+    const price = 456;
+
+    const result = underTest.formatPriceWithoutCurrency(price);
+
+    expect(result).toBe('456.-');
+  });
+});
+
+describe('formatPriceWithCurrency', () => {
+  test('should format price with CHF in front', () => {
+    const price = 456;
+
+    const result = underTest.formatPriceWithCurrency(price);
+
+    expect(result).toBe('CHF 456.-');
+  });
+});

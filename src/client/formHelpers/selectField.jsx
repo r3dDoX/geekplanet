581dea4d059daa7f6807930b229f97752c6bc3be
r3dDoX <patrick@walther-online.ch>
2017-04-15 18:39:49 +0200

    revert rest param for select field

--- src/client/formHelpers/selectField.jsx
+++ src/client/formHelpers/selectField.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import SelectField from 'material-ui/SelectField';
 
 // eslint-disable-next-line
-export default ({ input, label, meta: { touched, error }, children, ...rest }) => (
+export default ({ input, label, style, meta: { touched, error }, children }) => (
   <SelectField
     floatingLabelText={label}
     errorText={touched && error}
-    onChange={(event, index, value) => input.onChange(value)}
-    autoWidth
     {...input}
-    {...rest}
+    onChange={(event, index, value) => input.onChange(value)}
+    style={style}
+    autoWidth
   >
     {children}
   </SelectField>

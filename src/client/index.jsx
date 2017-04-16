5235c29e49deff7b9826902feaf790c919b9d44b
r3dDoX <patrick@walther-online.ch>
2017-04-16 18:24:53 +0200

    move internationalisation to layout component

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -4,11 +4,16 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { addLocaleData } from 'react-intl';
+import de from 'react-intl/locale-data/de';
 import App from './app.jsx';
 import geekplanetTheme from './theme';
 import reducers from './reducers';
 
 injectTapEventPlugin();
+
+addLocaleData([...de]);
+
 const store = createStore(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()

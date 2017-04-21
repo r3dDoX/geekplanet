54eba24897731352d5b4662bb45f7060597fc400
r3dDoX <r3dDoX@users.noreply.github.com>
2017-04-21 23:06:33 +0200

    use eval for better build performance

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -6,7 +6,7 @@ const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common.config');
 
 module.exports = merge(commonConfig, {
-  devtool: 'source-map',
+  devtool: 'eval',
 
   entry: {
     main: [

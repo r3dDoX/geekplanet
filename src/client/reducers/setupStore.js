ff77072402fd8e5d5c29a8c3673b065501463399
r3dDoX <r3dDoX@users.noreply.github.com>
2017-05-14 22:28:37 +0200

    apply thunk middleware

--- src/client/reducers/setupStore.js
+++ src/client/reducers/setupStore.js
@@ -1,10 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './root.reducer';
 
 export default () => {
   const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
   );
 
   if (module.hot) {

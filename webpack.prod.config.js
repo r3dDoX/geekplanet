5c0b714eb08db371337a8bfd1ecdba2440fb7e9e
r3dDoX <r3dDoX@users.noreply.github.com>
2017-05-28 15:18:01 +0200

    change from runtime compression to compile time compression

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const CompressionPlugin = require('compression-webpack-plugin');
 const commonConfig = require('./webpack.common.config');
 
 module.exports = merge(commonConfig, {
@@ -17,5 +18,12 @@ module.exports = merge(commonConfig, {
     new webpack.DefinePlugin(
       require(`./src/config/${process.env.CONFIG}.config.json`)
     ),
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
   ],
 });

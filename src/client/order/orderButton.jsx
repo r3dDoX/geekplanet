cea8e5730b02c07f0354411a4d40600ba390ed24
r3dDoX <r3dDoX@users.noreply.github.com>
2017-06-23 17:29:51 +0200

    use import child directory for smaller bundle size

--- src/client/order/orderButton.jsx
+++ src/client/order/orderButton.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { RaisedButton } from 'material-ui';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import RaisedButton from 'material-ui/RaisedButton';
 import ShoppingCart from 'material-ui/svg-icons/action/add-shopping-cart';
 import { createAddItemToShoppingCart } from '../actions';
 import { ProductPropType } from '../propTypes';

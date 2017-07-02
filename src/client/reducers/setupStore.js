8fb45667c4c7a53e1d3a5aaa9c1015a4c8333849
r3dDoX <patrick@walther-online.ch>
2017-07-02 21:30:11 +0200

    Revert: use compose to apply store enhancers (9e9cddffa64787078a43db432caccd0f40bd71f5) * fix error when no redux dev tools available

--- src/client/reducers/setupStore.js
+++ src/client/reducers/setupStore.js
@@ -1,14 +1,12 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './root.reducer';
 
 export default () => {
   const store = createStore(
     rootReducer,
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    )
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
   );
 
   if (module.hot) {

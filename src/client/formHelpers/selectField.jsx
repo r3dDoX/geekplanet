418f4ca6fe9f91ca1dffb11ad396f3c8ca39f77f
r3dDoX <r3dDoX@users.noreply.github.com>
2017-07-27 20:09:57 +0200

    implement correct product loading and filling

--- src/client/formHelpers/selectField.jsx
+++ src/client/formHelpers/selectField.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import SelectField from 'material-ui/SelectField';
 
 // eslint-disable-next-line
-export default ({ input, label, style, meta: { touched, error }, children }) => (
+export default ({ input, label, style, selectedValue = '', meta: { touched, error }, children }) => (
   <SelectField
     floatingLabelText={label}
     errorText={touched && error}
     {...input}
+    value={selectedValue}
     onChange={(event, index, value) => input.onChange(value)}
     style={style}
     autoWidth

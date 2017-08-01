1178624480b115e0832427a1acfb081fa9816998
r3dDoX <r3dDoX@users.noreply.github.com>
2017-08-01 14:09:19 +0200

    don't let value be set at all if omitted * problem with react-forms not updating the value when set

--- src/client/formHelpers/selectField.jsx
+++ src/client/formHelpers/selectField.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import SelectField from 'material-ui/SelectField';
 
 // eslint-disable-next-line
-export default ({ input, label, style, selectedValue = '', multiple = false, meta: { touched, error }, children }) => (
+export default ({ input, label, style, selectedValue, multiple = false, meta: { touched, error }, children }) => (
   <SelectField
     floatingLabelText={label}
     errorText={touched && error}
     {...input}
-    value={selectedValue}
+    {...selectedValue ? { value: selectedValue } : {}}
     onChange={(event, index, value) => input.onChange(value)}
     style={style}
     autoWidth

3eeaacfd08b0e5c727587a367fc66b143b90b88b
r3dDoX <patrick@walther-online.ch>
2017-08-08 22:41:46 +0200

    Revert "fix deprecated mongoose methods"
    This reverts commit 4dce797

--- src/server/db/mongoHelper.js
+++ src/server/db/mongoHelper.js
@@ -3,11 +3,11 @@ const Grid = require('gridfs-stream');
 
 const mongoURI = process.env.MONGODB_URI || process.env.MONGOHQ_URL;
 mongoose.Promise = Promise;
-mongoose.connect(mongoURI, { useMongoClient: true });
+mongoose.connect(mongoURI);
 
 const mongoHelper = {
   init() {
-    const connection = mongoose.createConnection(mongoURI, { useMongoClient: true });
+    const connection = mongoose.createConnection(mongoURI);
     connection.once('open', () => {
       mongoHelper.gridfs = Grid(connection.db, mongoose.mongo);
     });

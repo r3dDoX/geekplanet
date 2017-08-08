4dce797e3caf96d7843b3ea94ab1a925a88052a0
r3dDoX <patrick@walther-online.ch>
2017-08-08 21:51:35 +0200

    fix deprecated mongoose methods

--- src/server/db/mongoHelper.js
+++ src/server/db/mongoHelper.js
@@ -3,11 +3,11 @@ const Grid = require('gridfs-stream');
 
 const mongoURI = process.env.MONGODB_URI || process.env.MONGOHQ_URL;
 mongoose.Promise = Promise;
-mongoose.connect(mongoURI);
+mongoose.connect(mongoURI, { useMongoClient: true });
 
 const mongoHelper = {
   init() {
-    const connection = mongoose.createConnection(mongoURI);
+    const connection = mongoose.createConnection(mongoURI, { useMongoClient: true });
     connection.once('open', () => {
       mongoHelper.gridfs = Grid(connection.db, mongoose.mongo);
     });

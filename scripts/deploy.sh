baf93103b70572a9d957374cefedfbe8ad847a48
r3dDoX <patrick@walther-online.ch>
2017-08-15 18:29:16 +0200

    uncomment heroku stuff

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-echo `pwd`
-
 # install heroku cli
-#wget -qO- https://toolbelt.heroku.com/install.sh | sh
+wget -qO- https://toolbelt.heroku.com/install.sh | sh
 
 # build and deploy docker image
-#heroku container:login --password=$1
-#heroku container:push web --app geekplanet-docker
+heroku container:login --password=$1
+heroku container:push web --app geekplanet-docker

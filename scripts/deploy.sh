8f78a3296521d404a7bd5296e93fd5fe0e942d51
r3dDoX <patrick@walther-online.ch>
2017-08-15 17:26:26 +0200

    try downloading heroku cli and deploy docker image

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,8 @@
-heroku container:login
+#!/usr/bin/env bash
+
+# install heroku cli
+wget -qO- https://toolbelt.heroku.com/install.sh | sh
+
+# build and deploy docker image
+heroku container:login --password=$1
+heroku container:push web --app geekplanet-docker

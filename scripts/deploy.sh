78e6087a0ead1e79b61afd33a0c8232e45352b89
r3dDoX <patrick@walther-online.ch>
2017-08-15 18:19:21 +0200

    comment stuff to search error

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,8 @@
 echo `pwd`
 
 # install heroku cli
-wget -qO- https://toolbelt.heroku.com/install.sh | sh
+#wget -qO- https://toolbelt.heroku.com/install.sh | sh
 
 # build and deploy docker image
-heroku container:login --password=$1
-heroku container:push web --app geekplanet-docker
+#heroku container:login --password=$1
+#heroku container:push web --app geekplanet-docker

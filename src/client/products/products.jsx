0f818f874643196d87915f201ccd5ccdf0550720
r3dDoX <r3dDoX@users.noreply.github.com>
2017-08-15 23:53:24 +0200

    Revert "show spinner while loading products"
    This reverts commit 0edbd3b

--- src/client/products/products.jsx
+++ src/client/products/products.jsx
@@ -1,10 +1,9 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { createLoadProducts } from '../actions';
-import MainSpinner from '../layout/mainSpinner.jsx';
 import ProductList from '../products/productList.jsx';
 import { ProductPropType } from '../propTypes';
+import { createLoadProducts } from '../actions';
 import ProductFilter from './productfilter/productFilter.jsx';
 
 class Products extends React.Component {
@@ -18,11 +17,7 @@ class Products extends React.Component {
     return (
       <div>
         <ProductFilter />
-        {this.props.products.length ? (
-          <ProductList products={this.props.filteredProducts} />
-        ) : (
-          <MainSpinner />
-        )}
+        <ProductList products={this.props.filteredProducts} />
       </div>
     );
   }

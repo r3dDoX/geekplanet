70b41979278ed3ed9d742a963fa351d091c6b373
r3dDoX <patrick@walther-online.ch>
2017-08-15 23:24:27 +0200

    build develop to geekplanet app

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker login -u=_ -p="$1" registry.heroku.com
-docker build . -t registry.heroku.com/geekplanet-docker/web
-docker push registry.heroku.com/geekplanet-docker/web
+docker build . -t registry.heroku.com/geekplanet/web
+docker push registry.heroku.com/geekplanet/web

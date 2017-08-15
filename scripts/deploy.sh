abb19f95e70d5a4bafeb33032417300097efa935
r3dDoX <patrick@walther-online.ch>
2017-08-15 22:47:58 +0200

    use plain docker

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# install heroku cli
-heroku --version
-
-# build and deploy docker image
-#heroku container:login --password=$1
-#heroku container:push web --app geekplanet-docker
+docker login -u $1 -p $2 registry.heroku.com
+docker push geekplanet-docker

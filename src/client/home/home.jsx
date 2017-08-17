3be96e4806b1a36386cd55790f171fb7cb28a9bd
r3dDoX <r3dDoX@users.noreply.github.com>
2017-08-17 17:27:51 +0200

    use media query to hide header when mobile

--- src/client/home/home.jsx
+++ src/client/home/home.jsx
@@ -1,40 +1,33 @@
-import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 import PropTypes from 'prop-types';
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { grey50 } from 'material-ui/styles/colors';
-import RaisedButton from 'material-ui/RaisedButton';
-import { FormattedMessage } from 'react-intl';
-import { backgroundColor } from '../theme';
-import ProductList from '../products/productList.jsx';
-import Slogan from './slogan.jsx';
-import { ProductPropType } from '../propTypes';
+import styled from 'styled-components';
 import { createLoadSpotlightProducts } from '../actions';
+import ProductList from '../products/productList.jsx';
+import { ProductPropType } from '../propTypes';
+import { backgroundColor } from '../theme';
+import Slogan from './slogan.jsx';
 
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    flexWrap: 'wrap',
-  },
-  header: {
-    padding: '80px 30px',
-    backgroundColor,
-  },
-  sloganContainer: {
-    maxWidth: '350px',
-    margin: '0 auto',
-  },
-  headerSubtitle: {
-    color: grey50,
-    fontSize: '18px',
-    fontWeight: 300,
-    margin: '20px auto 50px',
-  },
-  productLinkButton: {
-    margin: '20px',
-  },
-};
+const Header = styled.div`
+  padding: 80px 30px;
+  background-color: ${backgroundColor};
+  
+  @media (max-width: 659px) {
+    display: none;
+  }
+`;
+
+const SloganContainer = styled.div`
+  max-width: 350px;
+  margin: 0 auto;
+`;
+
+const ProductLinkButton = styled(RaisedButton)`
+  margin: 20px;
+`;
 
 class Home extends React.Component {
   componentWillMount() {
@@ -48,15 +41,14 @@ class Home extends React.Component {
 
     return (
       <div>
-        <div style={styles.header}>
-          <div style={styles.sloganContainer}>
+        <Header>
+          <SloganContainer>
             <Slogan />
-          </div>
-        </div>
+          </SloganContainer>
+        </Header>
         <ProductList products={spotlightProducts} />
-        <RaisedButton
+        <ProductLinkButton
           primary
-          style={styles.productLinkButton}
           label={<FormattedMessage id="PRODUCT.TO_PRODUCTS" />}
           containerElement={<Link to="/products" />}
         />

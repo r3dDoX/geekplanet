62589d3e33a3ff1a6afa35a0eb4e0c4846fefcc1
r3dDoX <patrick@walther-online.ch>
2017-08-19 15:42:53 +0200

    add filtering by tags and priority of filters

--- src/client/products/products.reducer.test.js
+++ src/client/products/products.reducer.test.js
@@ -3,27 +3,103 @@ import { FILTER_PRODUCTS, TOGGLE_FILTER_CATEGORY } from '../actions';
 
 describe('Products Reducer', () => {
   describe('FILTER_PRODUCTS', () => {
-    it('should filter products by german name and shortDescription', () => {
+    it('should filter products by german name', () => {
       const state = {
         categoriesToFilter: [],
+        productFilters: {},
         products: [
           {
             de: {
-              name: 'Product abc',
+              name: 'Product Abc',
               shortDescription: 'shortDescription',
             },
-          },
-          {
-            de: {
-              name: 'name',
-              shortDescription: 'some abc product',
-            },
+            tags: [],
           },
           {
             de: {
               name: 'name',
               shortDescription: 'shortDescription',
             },
+            tags: [],
+          },
+        ],
+      };
+      const action = {
+        type: FILTER_PRODUCTS,
+        filterString: 'abc',
+      };
+
+      const newState = underTest(state, action);
+
+      expect(newState.filteredProducts).toHaveLength(1);
+      expect(newState.filteredProducts[0].de.name).toBe('Product Abc');
+    });
+
+    it('should filter products by german shortDescription', () => {
+      const state = {
+        categoriesToFilter: [],
+        productFilters: {},
+        products: [
+          {
+            de: {
+              name: 'name',
+              shortDescription: 'Product aBc',
+            },
+            tags: [],
+          },
+          {
+            de: {
+              name: 'name',
+              shortDescription: 'shortDescription',
+            },
+            tags: [],
+          },
+        ],
+      };
+      const action = {
+        type: FILTER_PRODUCTS,
+        filterString: 'abc',
+      };
+
+      const newState = underTest(state, action);
+
+      expect(newState.filteredProducts).toHaveLength(1);
+      expect(newState.filteredProducts[0].de.shortDescription).toBe('Product aBc');
+    });
+
+    it('should filter products by tags', () => {
+      const state = {
+        categoriesToFilter: [],
+        productFilters: {},
+        products: [
+          {
+            de: {
+              name: 'name',
+              shortDescription: 'shortDescription',
+            },
+            tags: [
+              'tag abc',
+              'tag efg',
+            ],
+          },
+          {
+            de: {
+              name: 'name',
+              shortDescription: 'shortDescription',
+            },
+            tags: [
+              'tag1',
+              'tag2',
+            ],
+          },
+          {
+            de: {
+              name: 'name',
+              shortDescription: 'shortDescription',
+            },
+            tags: [
+              'aBc',
+            ],
           },
         ],
       };
@@ -35,8 +111,45 @@ describe('Products Reducer', () => {
       const newState = underTest(state, action);
 
       expect(newState.filteredProducts).toHaveLength(2);
-      expect(newState.filteredProducts[0].de.name).toBe('Product abc');
-      expect(newState.filteredProducts[1].de.shortDescription).toBe('some abc product');
+      expect(newState.filteredProducts[0].tags[0]).toBe('tag abc');
+      expect(newState.filteredProducts[1].tags[0]).toBe('aBc');
+    });
+
+    it('should update filter on second action', () => {
+      const state = {
+        categoriesToFilter: [],
+        productFilters: {},
+        products: [
+          {
+            de: {
+              name: 'Product efg',
+              shortDescription: 'shortDescription',
+            },
+            tags: [],
+          },
+          {
+            de: {
+              name: 'name',
+              shortDescription: 'some ef product',
+            },
+            tags: [],
+          },
+        ],
+      };
+      const action = {
+        type: FILTER_PRODUCTS,
+        filterString: 'ef',
+      };
+      const action2 = {
+        type: FILTER_PRODUCTS,
+        filterString: 'efg',
+      };
+
+      const intermediateState = underTest(state, action);
+      const newState = underTest(intermediateState, action2);
+
+      expect(newState.filteredProducts).toHaveLength(1);
+      expect(newState.filteredProducts[0].de.name).toBe('Product efg');
     });
   });
 
@@ -44,6 +157,7 @@ describe('Products Reducer', () => {
     it('should filter products by category ids', () => {
       const state = {
         categoriesToFilter: [],
+        productFilters: {},
         products: [
           {
             category: 'categoryId0',
@@ -66,6 +180,50 @@ describe('Products Reducer', () => {
 
       expect(newState.filteredProducts).toHaveLength(1);
       expect(newState.filteredProducts[0].category).toBe('categoryId1');
+    });
+
+    it('should update filter on second action', () => {
+      const state = {
+        categoriesToFilter: [],
+        productFilters: {},
+        products: [
+          {
+            category: 'categoryId0',
+          },
+          {
+            category: 'categoryId1',
+          },
+          {
+            category: 'categoryId2',
+          },
+        ],
+      };
+      const action = {
+        type: TOGGLE_FILTER_CATEGORY,
+        productCategories: [
+          {
+            _id: 'categoryId1',
+          },
+        ],
+      };
+      const action2 = {
+        type: TOGGLE_FILTER_CATEGORY,
+        productCategories: [
+          {
+            _id: 'categoryId1',
+          },
+          {
+            _id: 'categoryId0',
+          },
+        ],
+      };
+
+      const intermediateState = underTest(state, action);
+      const newState = underTest(intermediateState, action2);
+
+      expect(newState.filteredProducts).toHaveLength(2);
+      expect(newState.filteredProducts[0].category).toBe('categoryId0');
+      expect(newState.filteredProducts[1].category).toBe('categoryId1');
     });
   });
 });

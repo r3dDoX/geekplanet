1d4e2c01d8b11d86bc1ee137a814a1ba8f517048
r3dDoX <r3dDoX@users.noreply.github.com>
2017-08-21 17:20:51 +0200

    make Docker build configurable

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker login -u=_ -p="$1" registry.heroku.com
-docker build . -t registry.heroku.com/geekplanet-staging/web
+docker build . --build-arg BUILD_ENV="staging" -t registry.heroku.com/geekplanet-staging/web
 docker push registry.heroku.com/geekplanet-staging/web
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker login -u=_ -p="$1" registry.heroku.com
-docker build . -t registry.heroku.com/geekplanet/web
+docker build . --build-arg BUILD_ENV="production" -t registry.heroku.com/geekplanet/web
 docker push registry.heroku.com/geekplanet/web

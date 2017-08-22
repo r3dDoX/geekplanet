142fb7c9eb7a9c3a14688dcc7da0c715c475c0a9
r3dDoX <patrick@walther-online.ch>
2017-08-22 22:30:50 +0200

    add some styling to orders table

--- src/client/admin/orders/orders.jsx
+++ src/client/admin/orders/orders.jsx
@@ -1,10 +1,30 @@
+import { grey200 } from 'material-ui/styles/colors';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 import { formatPriceWithCurrency } from '../../../common/priceFormatter';
 import { OrdersPropType } from '../../propTypes';
 import { createLoadOrders } from '../adminActions';
+
+const Container = styled.div`
+  overflow-X: auto;
+`;
+
+const OrderTable = styled.table`
+  width: 100%;
+  padding: 20px;
+  
+  tbody tr:nth-child(2n - 1) {
+    background-color: ${grey200};
+    
+  }
+  
+  td {
+    padding: 5px;
+  }
+`;
 
 class Orders extends React.Component {
   componentWillMount() {
@@ -17,60 +37,62 @@ class Orders extends React.Component {
     const { orders } = this.props;
 
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>
-              <FormattedMessage id="ORDERS.ID" />
-            </th>
-            <th>
-              <FormattedMessage id="ORDERS.STATUS" />
-            </th>
-            <th>
-              <FormattedMessage id="ORDERS.ADDRESS" />
-            </th>
-            <th>
-              <FormattedMessage id="ORDERS.ITEMS" />
-            </th>
-            <th>
-              <FormattedMessage id="ORDERS.TOTAL" />
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map(order => (
-            <tr key={order._id}>
-              <td>
-                {order._id}
-              </td>
-              <td>
-                {order.state}
-              </td>
-              <td>
-                {`${order.address.title} ${order.address.firstName} ${order.address.lastName}`}<br />
-                {order.address.streetAddress}<br />
-                {`${order.address.zip} ${order.address.city}`}<br />
-                {order.address.country}
-              </td>
-              <td>
-                <ul>
-                  {order.items.map(item => (
-                    <li key={item.product._id}>
-                      {`${item.amount} * ${item.product.de.name}`}
-                    </li>
-                  ))}
-                </ul>
-              </td>
-              <td>
-                {formatPriceWithCurrency(order.items.reduce(
-                  (sum, { amount, product }) => sum + (amount * product.price),
-                  0
-                ))}
-              </td>
+      <Container>
+        <OrderTable cellPadding="0" cellSpacing="0">
+          <thead>
+            <tr>
+              <th>
+                <FormattedMessage id="ORDERS.ID" />
+              </th>
+              <th>
+                <FormattedMessage id="ORDERS.STATUS" />
+              </th>
+              <th>
+                <FormattedMessage id="ORDERS.ADDRESS" />
+              </th>
+              <th>
+                <FormattedMessage id="ORDERS.ITEMS" />
+              </th>
+              <th>
+                <FormattedMessage id="ORDERS.TOTAL" />
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {orders.map(order => (
+              <tr key={order._id}>
+                <td>
+                  {order._id}
+                </td>
+                <td>
+                  {order.state}
+                </td>
+                <td>
+                  {`${order.address.title} ${order.address.firstName} ${order.address.lastName}`}<br />
+                  {order.address.streetAddress}<br />
+                  {`${order.address.zip} ${order.address.city}`}<br />
+                  {order.address.country}
+                </td>
+                <td>
+                  <ul>
+                    {order.items.map(item => (
+                      <li key={item.product._id}>
+                        {`${item.amount} * ${item.product.de.name}`}
+                      </li>
+                    ))}
+                  </ul>
+                </td>
+                <td>
+                  {formatPriceWithCurrency(order.items.reduce(
+                    (sum, { amount, product }) => sum + (amount * product.price),
+                    0
+                  ))}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </OrderTable>
+      </Container>
     );
   }
 }

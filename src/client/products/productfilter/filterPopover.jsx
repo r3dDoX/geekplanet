262fca317f1d7563226e90243cbd221c5f93ea0d
r3dDoX <patrick@walther-online.ch>
2017-09-02 13:03:22 +0200

    implement mobile view

--- src/client/products/productfilter/filterPopover.jsx
+++ src/client/products/productfilter/filterPopover.jsx
@@ -1,21 +1,38 @@
 import RaisedButton from 'material-ui/RaisedButton';
+import { grey200, grey800 } from 'material-ui/styles/colors';
+import CloseIcon from 'material-ui/svg-icons/content/clear';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
+import { mdMinSize, xsMaxSize } from '../../theme';
+import { FormattedMessage } from 'react-intl';
 
 const Popover = styled.div`
   position: fixed;
-  z-index: 2;
+  z-index: 1101;
   background-color: rgba(255, 255, 255, 0.8);
   overflow-y: auto;
   left: 0;
   width: 100%;
-  animation: .25s linear 0s slideDown;
-  animation-fill-mode: both;
+  
+  @media screen and (min-width: ${mdMinSize}) {
+    animation: .25s ease-in 0s slideDown;
+    animation-fill-mode: both;
+  }
+  
+  @media screen and (max-width: ${xsMaxSize}) {
+    animation: .25s ease-in 0s slideUp;
+    bottom: 0;
+  }
   
   @keyframes slideDown {
     from { bottom: 100%; }
     to { bottom: 0; }
+  }
+  
+  @keyframes slideUp {
+    from { top: 100%; }
+    to { top: 0; }
   }
 `;
 
@@ -47,26 +64,58 @@ const FilterButtonContainer = styled.div`
   }
 `;
 
+const PopoverTitleContainer = styled.div`
+  background-color: ${grey200};
+  display: flex;
+  align-items: center;
+
+  @media screen and (min-width: ${mdMinSize}) {
+    display: none;
+  }
+`;
+
+const PopoverTitle = styled.h2`
+  margin: 0;
+  padding: 0 20px;
+  font-weight: 400;
+  flex: 1 1 auto;
+  color: ${grey800};
+`;
+
+const PopoverCloseContainer = styled.div`
+  flex: none;
+  padding: 20px;
+  border-left: 1px solid ${grey800};
+`;
+
 const FilterButton = styled(RaisedButton)`
   margin-right: 20px;
 `;
 
 const FilterPopover = ({
-  anchorElementRect,
   children,
   toggleFilterView,
+  top,
 }) => (
   <Popover
     style={{
-      top: `${anchorElementRect.top + anchorElementRect.height + 5}px`,
+      top: `${top}px`,
     }}
   >
+    <PopoverTitleContainer>
+      <PopoverTitle>
+        <FormattedMessage id="PRODUCT_FILTER.TITLE" />
+      </PopoverTitle>
+      <PopoverCloseContainer onClick={toggleFilterView}>
+        <CloseIcon color={grey800} />
+      </PopoverCloseContainer>
+    </PopoverTitleContainer>
     <PopoverInlay>
       {children}
       <FilterButtonContainer>
         <FilterButton
           primary
-          label="Filter speichern"
+          label={<FormattedMessage id="PRODUCT_FILTER.BACK" />}
           onClick={toggleFilterView}
         />
       </FilterButtonContainer>
@@ -75,12 +124,7 @@ const FilterPopover = ({
 );
 
 FilterPopover.propTypes = {
-  anchorElementRect: PropTypes.shape({
-    left: PropTypes.number,
-    top: PropTypes.number,
-    width: PropTypes.number,
-    height: PropTypes.number,
-  }).isRequired,
+  top: PropTypes.number.isRequired,
   children: PropTypes.any.isRequired, // eslint-disable-line react/forbid-prop-types
   toggleFilterView: PropTypes.func.isRequired,
 };

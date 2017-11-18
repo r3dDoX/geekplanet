eff1e3e5c9b3f23efcf6698912b3910d8e079b61
r3dDoX <patrick@walther-online.ch>
2017-11-18 16:52:06 +0100

    add buttons for next and previous page

--- src/client/products/productList.jsx
+++ src/client/products/productList.jsx
@@ -1,8 +1,10 @@
+import RaisedButton from 'material-ui/RaisedButton';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import InfiniteScroll from 'react-infinite-scroller';
-import { withRouter } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
+import { Link, withRouter } from 'react-router-dom';
 import styled from 'styled-components';
 import MainSpinner from '../layout/mainSpinner.jsx';
 import { ProductPropType } from '../propTypes';
@@ -18,6 +20,13 @@ const ProductListContainer = styled.div`
   @media screen and (min-width: ${mdMinSize}) {
     padding-top: 90px;
   }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
 `;
 
 const PAGE_SIZE = 40;
@@ -81,14 +90,16 @@ class ProductList extends React.Component {
   }
 
   render() {
+    const previousLink = this.state.currentPage > 1 ? `${APP.BASE_URL}/products?page=${this.state.currentPage - 1}` : undefined;
+    const nextLink = (this.state.currentPage < this.props.products.length / PAGE_SIZE) ? `${APP.BASE_URL}/products?page=${this.state.currentPage + 1}` : undefined;
+
     return [
       <Helmet key="productListHead">
-        <base href={APP.BASE_URL} />
-        {this.state.currentPage > 1 &&
-          <link rel="prev" href={`/products?page=${this.state.currentPage - 1}`} />
+        {previousLink &&
+          <link rel="prev" href={previousLink} />
         }
-        {this.state.currentPage < this.props.products.length / PAGE_SIZE &&
-          <link rel="next" href={`/products?page=${this.state.currentPage + 1}`} />
+        {nextLink &&
+          <link rel="next" href={nextLink} />
         }
       </Helmet>,
       this.state.preselectedPage ? (
@@ -118,6 +129,33 @@ class ProductList extends React.Component {
             ))}
           </ProductListContainer>
         </InfiniteScroll>
+      ),
+      (this.state.preselectedPage) && (
+        <ButtonContainer>
+          {previousLink &&
+            <RaisedButton
+              primary
+              label={<FormattedMessage id="PRODUCT.PREVIOUS" />}
+              containerElement={
+                <a href={previousLink}>
+                  <FormattedMessage id="PRODUCT.PREVIOUS" />
+                </a>
+              }
+            />
+          }
+          &nbsp;
+          {nextLink &&
+          <RaisedButton
+            primary
+            label={<FormattedMessage id="PRODUCT.NEXT" />}
+            containerElement={
+              <a href={nextLink}>
+                <FormattedMessage id="PRODUCT.NEXT" />
+              </a>
+            }
+          />
+          }
+        </ButtonContainer>
       ),
     ];
   }

9c114ccc896e6e593e62f89e3c2c835c5d534b52
r3dDoX <r3dDoX@users.noreply.github.com>
2018-03-24 19:16:13 +0100

    use array return instead of wrapper div

--- src/client/shoppingcart/shoppingCartItem.jsx
+++ src/client/shoppingcart/shoppingCartItem.jsx
@@ -26,34 +26,37 @@ const ShoppingCartItem = ({
   shoppingCartItem,
   setAmount,
   locale,
-}) => (
-  <div>
-    <ListItem
-      primaryText={shoppingCartItem.product[locale].name}
-      secondaryText={<AmountAdjuster shoppingCartItem={shoppingCartItem} setAmount={setAmount} />}
-      leftAvatar={<Avatar
-        style={styles.avatar}
-        src={(shoppingCartItem.product.files.length) ?
-          `/api/products/pictures/${shoppingCartItem.product.files[0]}_s` : '/assets/images/notFound.jpg'
-        }
-      />}
-      rightAvatar={
-        <Avatar
-          color={pinkA400}
-          backgroundColor={transparent}
-        >
-          {formatPriceWithoutCurrency(shoppingCartItem.product.price * shoppingCartItem.amount)}
-        </Avatar>
+}) => [
+  <ListItem
+    key={`${shoppingCartItem.product._id}_item`}
+    primaryText={shoppingCartItem.product[locale].name}
+    secondaryText={<AmountAdjuster shoppingCartItem={shoppingCartItem} setAmount={setAmount} />}
+    leftAvatar={<Avatar
+      style={styles.avatar}
+      src={(shoppingCartItem.product.files.length) ?
+        `/api/products/pictures/${shoppingCartItem.product.files[0]}_s` : '/assets/images/notFound.jpg'
       }
-      innerDivStyle={styles.productName}
-    />
-    {shoppingCartItem.product.stock <= 0 ? (
-      <Subheader inset style={styles.notInStockMessage}>
-        <FormattedMessage id="SHOPPING_CART.NOT_IN_STOCK" />
-      </Subheader>
-    ) : null}
-  </div>
-);
+    />}
+    rightAvatar={
+      <Avatar
+        color={pinkA400}
+        backgroundColor={transparent}
+      >
+        {formatPriceWithoutCurrency(shoppingCartItem.product.price * shoppingCartItem.amount)}
+      </Avatar>
+    }
+    innerDivStyle={styles.productName}
+  />,
+  shoppingCartItem.product.stock <= 0 && (
+    <Subheader
+      key={`${shoppingCartItem.product._id}_stock`}
+      inset
+      style={styles.notInStockMessage}
+    >
+      <FormattedMessage id="SHOPPING_CART.NOT_IN_STOCK" />
+    </Subheader>
+  ),
+];
 
 ShoppingCartItem.propTypes = {
   shoppingCartItem: ShoppingCartItemPropType.isRequired,

7f0b82c6d6477a5134edb6e9e60b471c3663d9ef
r3dDoX <r3dDoX@users.noreply.github.com>
2018-03-24 19:37:39 +0100

    show applied coupons

--- src/client/shoppingcart/coupons.jsx
+++ src/client/shoppingcart/coupons.jsx
@@ -1,16 +1,25 @@
-import { Divider, Subheader } from 'material-ui';
+import Divider from 'material-ui/Divider';
+import MenuItem from 'material-ui/MenuItem';
+import Subheader from 'material-ui/Subheader';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { formatPriceWithoutCurrency } from '../../common/priceFormatter';
 import { CouponPropType } from '../propTypes';
 import AddCoupon from './addCoupon.jsx';
 
 
-const Coupons = ({ onAdd }) => [
+const Coupons = ({ coupons, onAdd }) => [
   <Divider key="couponsDivider" />,
   <Subheader key="couponsHeader" inset>
     <FormattedMessage id="COUPONS.TITLE" />
   </Subheader>,
+  coupons.map(coupon => (
+    <MenuItem
+      insetChildren primaryText={coupon._id}
+      secondaryText={`- ${formatPriceWithoutCurrency(coupon.amount)}`}
+    />
+  )),
   <AddCoupon key="couponAddForm" onAdd={onAdd} />,
 ];
 

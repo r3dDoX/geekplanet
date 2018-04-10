3998dda61411504646bf29a1b90f192327edeeb9
r3dDoX <patrick@walther-online.ch>
2018-04-10 20:41:56 +0200

    add nothing found message

---
+++ src/client/products/nothingFound.jsx
@@ -0,0 +1,22 @@
+import { grey500 } from 'material-ui/styles/colors';
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import styled from 'styled-components';
+import { mdMinSize } from '../theme';
+
+const Container = styled.p`
+  margin: 0;
+  padding: 25px 15px;
+  color: ${grey500};
+  text-align: center;
+  
+  @media screen and (min-width: ${mdMinSize}) {
+    padding-top: 90px;
+  }
+`;
+
+export default () => (
+  <Container>
+    <FormattedMessage id="PRODUCT_FILTER.NOTHING_FOUND" />
+  </Container>
+);

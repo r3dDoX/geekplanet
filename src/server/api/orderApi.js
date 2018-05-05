9a68afa069f5753fe3c363c7c19b5a45d3de4b94
r3dDoX <r3dDoX@users.noreply.github.com>
2018-05-05 20:04:12 +0200

    make usage of async await

--- src/server/api/orderApi.js
+++ src/server/api/orderApi.js
@@ -2,12 +2,11 @@ const fs = require('fs');
 const { authorization, isAdmin } = require('./auth');
 const bodyParser = require('body-parser');
 const asyncHandler = require('express-async-handler');
-const Logger = require('../logger');
 const envConfig = require('../../config/envConfig');
 const stripe = require('stripe')(envConfig.getSecretKey('PAYMENT_SECRET'));
 const esrGenerator = require('../esr/esrGenerator');
 const mail = require('../email/mail');
-const { saveOrUpdate, handleGenericError } = require('../db/mongoHelper');
+const { saveOrUpdate } = require('../db/mongoHelper');
 const esrCodeHelpers = require('../esr/esrCodeHelpers');
 
 const orderConfig = envConfig.getEnvironmentSpecificConfig().ORDER;
@@ -37,26 +36,20 @@ function updateCoupons(itemTotal, coupons) {
   ));
 }
 
-function createAndSendEsr(order, email) {
+async function createAndSendEsr(order, email) {
   const { address, total } = order;
 
-  return Invoice
-    .findOneAndUpdate(
-      { _id: order.invoice },
-      { $set: { value: total, address } },
-      { new: true })
-    .then((invoice) => {
-      const esr = esrCodeHelpers.generateInvoiceNumberCode(invoice.invoiceNumber);
+  const invoice = Invoice.findOneAndUpdate(
+    { _id: order.invoice },
+    { $set: { value: total, address } },
+    { new: true });
 
-      return esrGenerator
-        .generate(esr, order._id, invoice.value, invoice.address)
-        .then(pdfPath => Promise.all([
-          mail.sendESR(order, email, pdfPath).then(() =>
-            fs.unlink(pdfPath), err => err && Logger(err)
-          ),
-          Invoice.findOneAndUpdate({ _id: invoice._id }, { $set: { esr } }),
-        ]));
-    });
+  const esr = esrCodeHelpers.generateInvoiceNumberCode(invoice.invoiceNumber);
+  const pdfPath = await esrGenerator.generate(esr, order._id, invoice.value, invoice.address);
+
+  await mail.sendESR(order, email, pdfPath);
+  fs.unlink(pdfPath);
+  await Invoice.findOneAndUpdate({ _id: invoice._id }, { $set: { esr } });
 }
 
 async function mapDbProductsToClientItems(clientItems) {
@@ -126,23 +119,20 @@ module.exports = {
       res.sendStatus(200);
     }));
 
-    app.post('/api/order/sent', authorization, isAdmin, bodyParser.json(), (req, res) =>
-      Order
-        .findOneAndUpdate({ _id: req.body.orderId }, { $set: { state: OrderState.SENT } })
-        .then(() => res.sendStatus(200))
-        .catch(error => handleGenericError(error, res))
-    );
+    app.post('/api/order/sent', authorization, isAdmin, bodyParser.json(),
+      asyncHandler(async (req, res) =>
+        Order
+          .findOneAndUpdate({ _id: req.body.orderId }, { $set: { state: OrderState.SENT } })
+          .then(() => res.sendStatus(200))));
 
-    app.put('/api/userAddress', authorization, bodyParser.json(), (req, res) =>
+    app.put('/api/userAddress', authorization, bodyParser.json(), asyncHandler(async (req, res) =>
       saveOrUpdate(UserAddress, Object.assign(req.body, { user: req.user.sub }))
-        .then(address => res.status(200).send(address._id))
-        .catch(error => handleGenericError(error, res)));
+        .then(address => res.status(200).send(address._id))));
 
-    app.get('/api/userAddresses', authorization, (req, res) =>
+    app.get('/api/userAddresses', authorization, asyncHandler(async (req, res) =>
       UserAddress
         .find({ user: req.user.sub }, { user: 0 })
-        .then(addresses => res.status(200).send(JSON.stringify(addresses)))
-        .catch(error => handleGenericError(error, res)));
+        .then(addresses => res.status(200).send(JSON.stringify(addresses)))));
 
     app.post('/api/payment/cleared', bodyParser.json(), authorization,
       asyncHandler(async (req, res) => {
@@ -162,46 +152,42 @@ module.exports = {
       })
     );
 
-    app.post('/api/payment/prepayment', bodyParser.json(), authorization, (req, res) =>
-      new Invoice({ user: req.user.sub })
-        .save()
-        .then(invoice =>
-          Order.findOneAndUpdate(
-            { _id: req.body.shoppingCartId, user: req.user.sub },
-            { $set: { state: OrderState.WAITING, invoice: invoice._id } },
-            { new: true })
-        )
-        .then((order) => {
-          updateCoupons(order.itemTotal, order.coupons);
-          updateProductStocks(order.items);
-          res.sendStatus(200);
+    app.post('/api/payment/prepayment', bodyParser.json(), authorization,
+      asyncHandler(async (req, res) => {
+        const invoice = await new Invoice({ user: req.user.sub }).save();
+        const updatedOrder = await Order.findOneAndUpdate(
+          { _id: req.body.shoppingCartId, user: req.user.sub },
+          { $set: { state: OrderState.WAITING, invoice: invoice._id } },
+          { new: true });
 
-          return createAndSendEsr(order, req.user.email);
-        })
-        .catch(error => handleGenericError(error, req.user.email))
-    );
+        await updateCoupons(updatedOrder.itemTotal, updatedOrder.coupons);
+        await updateProductStocks(updatedOrder.items);
+        res.sendStatus(200);
+
+        await createAndSendEsr(updatedOrder, req.user.email);
+      }));
 
     app.post('/api/payment/prepayment/cleared', bodyParser.json(), authorization, isAdmin,
-      (req, res) => Order
+      asyncHandler(async (req, res) => Order
         .findOneAndUpdate(
           { _id: req.body.orderId },
           { $set: { state: OrderState.FINISHED } })
         .then(() => res.sendStatus(200))
-        .catch(error => handleGenericError(error, req.user.email))
-    );
+      ));
 
-    app.post('/api/payment/none', bodyParser.json(), authorization, (req, res) =>
-      Order.findOneAndUpdate(
-        { _id: req.body.shoppingCartId, user: req.user.sub, total: 0 },
-        { $set: { state: OrderState.FINISHED } },
-        { new: true }
-      )
-        .then((order) => {
-          updateCoupons(order.itemTotal, order.coupons);
-          res.sendStatus(200);
-          return mail.sendConfirmation(order, req.user.email);
-        })
-        .catch(error => handleGenericError(error, req.user.email))
-    );
+    app.post('/api/payment/none', bodyParser.json(), authorization,
+      asyncHandler(async (req, res) => {
+        const order = await Order.findOneAndUpdate(
+          { _id: req.body.shoppingCartId, user: req.user.sub, total: 0 },
+          { $set: { state: OrderState.FINISHED } },
+          { new: true }
+        );
+
+        await updateCoupons(order.itemTotal, order.coupons);
+        await updateProductStocks(order.items);
+        res.sendStatus(200);
+
+        await mail.sendConfirmation(order, req.user.email);
+      }));
   },
 };

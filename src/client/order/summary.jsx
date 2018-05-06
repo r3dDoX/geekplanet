00887494dd3e545c6a5edbf71bd00a2023bf3de9
r3dDoX <patrick@walther-online.ch>
2018-05-06 23:54:04 +0200

    add basic summary table

--- src/client/order/summary.jsx
+++ src/client/order/summary.jsx
@@ -1,14 +1,51 @@
 import RaisedButton from 'material-ui/RaisedButton';
+import { grey300, grey600 } from 'material-ui/styles/colors';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
+import { formatPriceWithCurrency } from '../../common/priceFormatter';
 import { createFinishOrder } from '../actions';
 import { CouponPropType, ShoppingCartItemsPropType } from '../propTypes';
+import PriceCalculation from '../../common/priceCalculation';
+
+const priceCalculation = PriceCalculation.create(ORDER.MIN_PRICE_SHIPPING, ORDER.SHIPPING_COST);
 
 const SummaryContainer = styled.div`
-  margin-top: 20px;
+  margin: 20px 0;
+`;
+
+const HeaderCell = styled.th`
+  padding: 10px;
+`;
+
+const TableBody = styled.tbody`
+  tr:nth-child(2n) {
+    background-color: ${grey300};
+  }
+  
+  tr:last-of-type td {
+    border-bottom: 1px solid ${grey600};
+  }
+`;
+
+const Cell = styled.td`
+  padding: 10px;
+`;
+
+const AmountCell = styled(Cell)`
+  text-align: center;
+`;
+
+const FooterTitleCell = styled(Cell)`
+  font-weight: bold;
+`;
+
+const GrandTotalRow = styled.tr`
+  td {
+    border-top: 1px solid ${grey600};
+  }
 `;
 
 const Summary = ({
@@ -16,18 +53,89 @@ const Summary = ({
   items,
   coupons,
   finishOrder,
-}) => [
-  <SummaryContainer key="orderSummary">
-    {items.map(item => <p key={item.product._id}>{item.product.de.name}</p>)}
-    {coupons.map(coupon => <p key={coupon._id}>{coupon._id}</p>)}
-  </SummaryContainer>,
-  <RaisedButton
-    key="finishOrderButton"
-    label={<FormattedMessage id="ORDER.SUMMARY.CONFIRM_ORDER" />}
-    primary
-    onClick={() => finishOrder(orderId)}
-  />,
-];
+}) => {
+  const itemTotal = priceCalculation.calculateItemTotal(items);
+
+  return [
+    <SummaryContainer key="orderSummary">
+      <table cellPadding={0} cellSpacing={0}>
+        <thead>
+          <tr>
+            <HeaderCell>Product</HeaderCell>
+            <HeaderCell>Amount</HeaderCell>
+            <HeaderCell>Price</HeaderCell>
+          </tr>
+        </thead>
+        <TableBody>
+          {items.map(item => (
+            <tr key={item.product._id}>
+              <Cell>
+                {item.product.de.name}
+              </Cell>
+              <AmountCell>
+                {item.amount}
+              </AmountCell>
+              <Cell>
+                {formatPriceWithCurrency(item.amount * item.product.price)}
+              </Cell>
+            </tr>
+          ))}
+        </TableBody>
+        <tfoot>
+          <tr>
+            <FooterTitleCell>Zwischentotal</FooterTitleCell>
+            <Cell />
+            <Cell>
+              {formatPriceWithCurrency(itemTotal)}
+            </Cell>
+          </tr>
+          {priceCalculation.isInShippingCostRange(itemTotal)
+            ? (
+              <tr>
+                <Cell>Versandkosten</Cell>
+                <AmountCell>+</AmountCell>
+                <Cell>
+                  {formatPriceWithCurrency(ORDER.SHIPPING_COST)}
+                </Cell>
+              </tr>
+            )
+            : null
+          }
+          {coupons.length
+            ? (
+              <tr>
+                <Cell>Gutscheine</Cell>
+                <AmountCell>-</AmountCell>
+                <Cell>
+                  {formatPriceWithCurrency(priceCalculation.calculateCouponsTotal(coupons))}
+                </Cell>
+              </tr>
+            )
+            : null
+          }
+          <GrandTotalRow>
+            <FooterTitleCell>Total</FooterTitleCell>
+            <Cell />
+            <Cell>
+              {formatPriceWithCurrency(
+                priceCalculation.calculateGrandTotal(
+                  itemTotal,
+                  coupons
+                )
+              )}
+            </Cell>
+          </GrandTotalRow>
+        </tfoot>
+      </table>
+    </SummaryContainer>,
+    <RaisedButton
+      key="finishOrderButton"
+      label={<FormattedMessage id="ORDER.SUMMARY.CONFIRM_ORDER" />}
+      primary
+      onClick={() => finishOrder(orderId)}
+    />,
+  ];
+};
 
 Summary.propTypes = {
   finishOrder: PropTypes.func.isRequired,

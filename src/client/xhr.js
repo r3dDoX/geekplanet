015580e353c839a6d7455f05c35340778ed6d104
r3dDoX <r3dDoX@users.noreply.github.com>
2018-05-07 17:46:32 +0200

    format try catch block more nicely

--- src/client/xhr.js
+++ src/client/xhr.js
@@ -14,11 +14,12 @@ function wrapXhrPromise(path, method, data, contentType) {
 
     request.addEventListener('load', () => {
       if (request.status === 200) {
-        let { response } = request;
+        const { response } = request;
         try {
-          response = JSON.parse(response);
-        } catch (e) { /* Nothing to do when no JSON returned */ }
-        resolve(response);
+          resolve(JSON.parse(response));
+        } catch (e) {
+          resolve(response);
+        }
       } else if (request.status === 401) {
         AuthService.logout();
         window.location.assign('/login');

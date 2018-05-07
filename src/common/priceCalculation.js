9f2d5059976c2c7125af90cd068a5433ca2fde80
r3dDoX <r3dDoX@users.noreply.github.com>
2018-05-07 08:12:16 +0200

    round after multiplication since rounding errors might occur

--- src/common/priceCalculation.js
+++ src/common/priceCalculation.js
@@ -1,14 +1,15 @@
 const PriceCalculation = {
   calculateItemTotal(items) {
     return items.reduce(
-      (acc, { amount, product }) => ((acc * 100) + ((amount * product.price) * 100)) / 100,
+      (acc, { amount, product }) =>
+        Math.round((acc * 100) + ((amount * product.price) * 100)) / 100,
       0
     );
   },
 
   calculateCouponsTotal(coupons) {
     return coupons.reduce(
-      (acc, { amount }) => ((acc * 100) + (amount * 100)) / 100,
+      (acc, { amount }) => Math.round((acc * 100) + (amount * 100)) / 100,
       0
     );
   },
@@ -29,21 +30,22 @@ const PriceCalculation = {
       return 0;
     }
 
-    return ((grandTotal * 100) - (couponsTotal * 100)) / 100;
+    return Math.round((grandTotal * 100) - (couponsTotal * 100)) / 100;
   },
 
   getRemainingCouponsAmount(itemTotal, coupons) {
     let total = this.isInShippingCostRange(itemTotal) ? itemTotal + this.shippingCost : itemTotal;
 
     return coupons.map((coupon) => {
+      let amount;
       if (coupon.amount > total) {
-        coupon.amount = ((coupon.amount * 100) - (total * 100)) / 100;
+        amount = Math.round((coupon.amount * 100) - (total * 100)) / 100;
       } else {
-        total = ((total * 100) - (coupon.amount * 100)) / 100;
-        coupon.amount = 0;
+        total = Math.round((total * 100) - (coupon.amount * 100)) / 100;
+        amount = 0;
       }
 
-      return coupon;
+      return Object.assign({}, coupon, { amount });
     });
   },
 };

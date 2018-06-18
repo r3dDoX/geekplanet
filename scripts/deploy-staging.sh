e467d53ed757bbb353f12d2c634b9fc66c5b7f73
r3dDoX <patrick@walther-online.ch>
2018-06-18 14:23:16 +0200

    output heroku version

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -3,3 +3,4 @@
 echo "$1" | docker login -u=_ --password-stdin registry.heroku.com
 docker build . --build-arg BUILD_ENV="staging" -t registry.heroku.com/geekplanet-staging/web
 docker push registry.heroku.com/geekplanet-staging/web
+heroku -v

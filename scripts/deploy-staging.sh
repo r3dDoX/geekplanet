43fa945fca34ce6e3c0dd88dc55ae4fe9ec8194d
r3dDoX <r3dDoX@users.noreply.github.com>
2018-06-19 06:49:37 +0200

    try heroku cli

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "$1" | docker login -u=_ --password-stdin registry.heroku.com
-docker build . --build-arg BUILD_ENV="staging" -t registry.heroku.com/geekplanet-staging/web
-docker push registry.heroku.com/geekplanet-staging/web
-heroku -v
+HEROKU_API_KEY="$1" heroku container:login
+docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
+heroku container:push web --app geekplanet-staging --arg BUILD_ENV="staging"

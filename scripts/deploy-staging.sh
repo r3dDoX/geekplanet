b8ce26e1113eee4ecfe7bb6505db1c1a01b1bd64
r3dDoX <patrick@walther-online.ch>
2018-06-19 07:19:11 +0200

    log out version after update

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 heroku update
+heroku -v
 HEROKU_API_KEY="$1" heroku container:login
 heroku container:push web --app geekplanet-staging --arg BUILD_ENV="staging"
 heroku container:release web --app geekplanet-staging

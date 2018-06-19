36bb0c84b9f26260d5a7e2b41720bc79ef4148ff
r3dDoX <r3dDoX@users.noreply.github.com>
2018-06-19 07:14:04 +0200

    try updating heroku-cli

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+heroku update
 HEROKU_API_KEY="$1" heroku container:login
 heroku container:push web --app geekplanet-staging --arg BUILD_ENV="staging"
 heroku container:release web --app geekplanet-staging

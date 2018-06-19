57274f0befb4efe0de0c31c183086901de9d6977
r3dDoX <patrick@walther-online.ch>
2018-06-19 06:54:38 +0200

    don't use docker cli anymore

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 HEROKU_API_KEY="$1" heroku container:login
-docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
 heroku container:push web --app geekplanet-staging --arg BUILD_ENV="staging"

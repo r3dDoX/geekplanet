ca95cb58e32c23ad7c83d1de11fc00a0aae1841d
r3dDoX <r3dDoX@users.noreply.github.com>
2018-06-19 21:18:27 +0200

    push prod builds to geekplanet app

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 HEROKU_API_KEY="$1" heroku container:login
-heroku container:push web --app geekplanet-staging --arg BUILD_ENV="production"
+heroku container:push web --app geekplanet --arg BUILD_ENV="production"

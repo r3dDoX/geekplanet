0de0d709d44070e44fd0b6dc8fb4cc40077db289
r3dDoX <patrick@walther-online.ch>
2018-06-24 18:12:05 +0200

    remove footer padding

--- src/client/layout/footer.jsx
+++ src/client/layout/footer.jsx
@@ -5,7 +5,6 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import Link from 'react-router-dom/Link';
 import styled from 'styled-components';
-import { laMinSize } from '../theme';
 
 const blueGrey100 = blueGrey['100'];
 const grey800 = grey['800'];
@@ -15,10 +14,6 @@ const Container = styled.div`
   align-items: flex-start;
   padding: 24px;
   background-color: ${blueGrey100};
-  
-  @media screen and (min-width: ${laMinSize}) {
-    padding-left: 280px;
-  }
 `;
 
 const TextContainer = styled.div`

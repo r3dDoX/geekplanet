9cb56593a68bf39942b050cc8a4937c161c71d2d
r3dDoX <r3dDoX@users.noreply.github.com>
2018-06-25 19:46:34 +0200

    fix spacing issues in shoppingcart drawer

--- src/client/shoppingcart/shoppingCartItem.jsx
+++ src/client/shoppingcart/shoppingCartItem.jsx
@@ -3,7 +3,6 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 import React from 'react';
@@ -24,10 +23,14 @@ const ProductImage = styled(Avatar)`
 `;
 
 const ProductText = styled(ListItemText)`
-  padding-right: 24px !important;
+  padding-right: 32px !important;
   span {
     text-align: justify;
   }
+`;
+
+const StyledListItemIcon = styled(ListItemIcon)`
+  margin-right: 0 !important;
 `;
 
 const ShoppingCartItem = ({
@@ -36,31 +39,28 @@ const ShoppingCartItem = ({
   locale,
 }) => [
   <ListItem key={`${shoppingCartItem.product._id}_item`}>
-    <ListItemIcon>
+    <StyledListItemIcon>
       <ProductImage
         src={(shoppingCartItem.product.files.length)
           ? `/api/products/pictures/${shoppingCartItem.product.files[0]}_s`
           : '/assets/images/notFound.jpg'
         }
       />
-    </ListItemIcon>
+    </StyledListItemIcon>
     <ProductText
       primary={shoppingCartItem.product[locale].name}
       secondary={<AmountAdjuster shoppingCartItem={shoppingCartItem} setAmount={setAmount} />}
     />
     <ListItemSecondaryAction>
-      <Price color="secondary" variant="headline">
+      <Price color="secondary" variant="subheading">
         {formatPriceWithoutCurrency(shoppingCartItem.product.price * shoppingCartItem.amount)}
       </Price>
     </ListItemSecondaryAction>
   </ListItem>,
   shoppingCartItem.product.stock <= 0 && (
-    <ListSubheader
-      key={`${shoppingCartItem.product._id}_stock`}
-      inset
-    >
-      <FormattedMessage id="SHOPPING_CART.NOT_IN_STOCK" />
-    </ListSubheader>
+    <ListItem key={`${shoppingCartItem.product._id}_stock`}>
+      <ListItemText inset secondary={<FormattedMessage id="SHOPPING_CART.NOT_IN_STOCK" />} />
+    </ListItem>
   ),
 ];
 

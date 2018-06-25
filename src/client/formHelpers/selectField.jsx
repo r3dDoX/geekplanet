ecd4787bfb33dee3c69c0ab94f520ad590a9c169
r3dDoX <r3dDoX@users.noreply.github.com>
2018-06-25 11:53:24 +0200

    migrate address form to new material-ui

--- src/client/formHelpers/selectField.jsx
+++ src/client/formHelpers/selectField.jsx
@@ -2,20 +2,39 @@ import React from 'react';
 import Select from '@material-ui/core/Select';
 import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
+import InputLabel from '@material-ui/core/InputLabel';
+import styled from 'styled-components';
 
-// eslint-disable-next-line
-export default ({ input, label, style, selectedValue, multiple = false, meta: { touched, error }, children }) => (
+const StyledSelect = styled(Select)`
+  min-width: 140px;
+`;
+
+/* eslint-disable react/prop-types */
+export default ({
+  input,
+  label,
+  selectedValue,
+  multiple = false,
+  meta: { touched, error },
+  children,
+}) => (
   <FormControl error={touched && error !== undefined}>
-    <Select
-      label={label}
+    <InputLabel htmlFor={input.name}>
+      {label}
+    </InputLabel>
+    <StyledSelect
       {...input}
       {...selectedValue ? { value: selectedValue } : {}}
-      onChange={(event, index, value) => input.onChange(value)}
+      onChange={event => input.onChange(event.target.value)}
       autoWidth
       multiple={multiple}
+      inputProps={{
+        name: input.name,
+        id: input.name,
+      }}
     >
       {children}
-    </Select>
+    </StyledSelect>
     {touched && error && (
       <FormHelperText>
         {error}
--- src/client/order/addressChooser.jsx
+++ src/client/order/addressChooser.jsx
@@ -1,35 +1,51 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
 import Divider from '@material-ui/core/Divider';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import styled from 'styled-components';
 import { UserAddressesPropType } from '../propTypes';
+
+const StyledSelect = styled(Select)`
+  min-width: 240px;
+`;
 
 const AddressChooser = ({
   addresses,
   selectAddress,
   selectedAddressId,
 }) => (
-  <Select
-    autoWidth
-    label={<FormattedMessage id="ORDER.ADDRESS.FORM.NO_ADDRESS" />}
-    onChange={(event, index, value) => selectAddress(value)}
-    value={selectedAddressId}
-  >
-    <MenuItem value="">
+  <FormControl>
+    <InputLabel htmlFor="address-chooser">
       <FormattedMessage id="ORDER.ADDRESS.FORM.NO_ADDRESS" />
-    </MenuItem>
-    <Divider />
-    {addresses.map(({ _id, streetAddress }) => (
-      <MenuItem
-        key={_id}
-        value={_id}
-      >
-        {streetAddress}
+    </InputLabel>
+    <StyledSelect
+      autoWidth
+      onChange={event => selectAddress(event.target.value)}
+      value={selectedAddressId}
+      inputProps={{
+        name: 'chooseAddress',
+        id: 'address-chooser',
+      }}
+      fullWidth
+    >
+      <MenuItem value="">
+        <FormattedMessage id="ORDER.ADDRESS.FORM.NO_ADDRESS" />
       </MenuItem>
-    ))}
-  </Select>
+      <Divider />
+      {addresses.map(({ _id, streetAddress }) => (
+        <MenuItem
+          key={_id}
+          value={_id}
+        >
+          {streetAddress}
+        </MenuItem>
+      ))}
+    </StyledSelect>
+  </FormControl>
 );
 
 AddressChooser.defaultProps = {

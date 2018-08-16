90f2a39763f0a504b923bd32d90435103ec2f426
r3dDoX <r3dDoX@users.noreply.github.com>
2018-08-16 19:45:04 +0200

    Revert "use docker login to fix basic auth deployment error"
    This reverts commit da7111b

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -2,5 +2,5 @@
 
 heroku -v
 
-echo $1 | docker login --username=_ --password-stdin registry.heroku.com
+HEROKU_API_KEY="$1" heroku container:login
 heroku container:push web --app geekplanet-staging --arg BUILD_ENV="staging"

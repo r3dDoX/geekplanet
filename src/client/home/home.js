327635bcad86bc4aa82892e55a37b66ebb5c348d
r3dDoX <r3dDoX@users.noreply.github.com>
2018-09-02 18:03:55 +0200

    extend responsive behaviour of column layout
    Signed-off-by: r3dDoX <patrick@walther-online.ch>

--- src/client/home/home.js
+++ src/client/home/home.js
@@ -168,9 +168,13 @@ const TileContainer = styled.div`
   flex: 1;
   padding: 15px;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-columns: repeat(auto-fill, minmax(450px, 1fr));
   grid-row-gap: 5px;
   grid-column-gap: 5px;
+  
+  @media screen and (max-width: ${theme.breakpoints.values.xl}px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
   
   @media screen and (max-width: ${theme.breakpoints.values.lg}px) {
     grid-template-columns: 1fr 1fr 1fr;

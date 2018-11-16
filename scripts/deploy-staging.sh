ef6b5fd0c87a0a02df73359a5ce2f9f17889c04a
r3dDoX <patrick@walther-online.ch>
2018-11-16 10:36:24 +0100

    update token

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -4,3 +4,4 @@ heroku -v
 
 HEROKU_API_KEY="$1" heroku container:login
 heroku container:push web --app geekplanet-staging --arg BUILD_ENV="staging"
+heroku container:release web --app geekplanet-staging

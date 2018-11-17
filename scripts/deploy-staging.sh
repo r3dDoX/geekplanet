7cd37c8dd5c75979ad45326863389ff43b5d99d8
r3dDoX <patrick@walther-online.ch>
2018-11-17 12:41:04 +0100

    log docker and heroku version

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+heroku -v
+docker -v
+
 HEROKU_API_KEY="$1" heroku container:login
 heroku container:push web --app geekplanet-staging --arg BUILD_ENV="staging"
 heroku container:release web --app geekplanet-staging

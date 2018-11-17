87925583a24ebe34a9977f305f706a178007a330
r3dDoX <r3dDoX@users.noreply.github.com>
2018-11-17 12:39:21 +0100

    remove version

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-heroku -v
-
 HEROKU_API_KEY="$1" heroku container:login
 heroku container:push web --app geekplanet-staging --arg BUILD_ENV="staging"
 heroku container:release web --app geekplanet-staging

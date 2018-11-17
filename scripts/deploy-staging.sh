693ec05fc9571434bf441f914e937a1b12e28708
r3dDoX <r3dDoX@users.noreply.github.com>
2018-11-17 12:31:11 +0100

    Revert "try without quotes"
    This reverts commit cac68a6

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-HEROKU_API_KEY=$1 heroku container:login
+heroku -v
+
+HEROKU_API_KEY="$1" heroku container:login
 heroku container:push web --app geekplanet-staging --arg BUILD_ENV="staging"
 heroku container:release web --app geekplanet-staging

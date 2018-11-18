1a952db35aab09d5278d6ca13c36cf533879d7a7
r3dDoX <r3dDoX@users.noreply.github.com>
2018-11-18 12:02:20 +0100

    try a netrc file for login procedure

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-cat ~/.netrc
-
-HEROKU_API_KEY="$1" heroku login
 echo "$1" | docker login -u=_ --password-stdin registry.heroku.com
 heroku container:push web --app geekplanet-staging --arg BUILD_ENV="staging"
 heroku container:release web --app geekplanet-staging

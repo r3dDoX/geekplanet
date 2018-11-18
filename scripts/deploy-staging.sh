6aa25d93e743c59040b425bc1e9516cbb0897553
r3dDoX <r3dDoX@users.noreply.github.com>
2018-11-18 12:06:33 +0100

    add encrypted file correctly

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cat .heroku-netrc >> $HOME/.netrc
 echo "$1" | docker login -u=_ --password-stdin registry.heroku.com
+
 heroku container:push web --app geekplanet-staging --arg BUILD_ENV="staging"
 heroku container:release web --app geekplanet-staging

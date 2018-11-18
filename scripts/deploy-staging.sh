1e3ca9d77df042daf19389b069a02972e20c76ab
r3dDoX <patrick@walther-online.ch>
2018-11-18 11:33:04 +0100

    separately login to docker registry

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-heroku -v
-docker -v
-
+echo "$1" | docker login -u=_ --password-stdin registry.heroku.com
 HEROKU_API_KEY="$1" heroku container:login
 heroku container:push web --app geekplanet-staging --arg BUILD_ENV="staging"
 heroku container:release web --app geekplanet-staging

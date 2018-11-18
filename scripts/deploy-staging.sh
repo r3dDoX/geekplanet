dd20a76024f2e86a9dc90d49b1ca91b8043d1bb3
r3dDoX <patrick@walther-online.ch>
2018-11-18 12:11:35 +0100

    move cat into before_deploy script and remove explicit docker login

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cat .heroku-netrc >> $HOME/.netrc
-echo "$1" | docker login -u=_ --password-stdin registry.heroku.com
-
+heroku container:login
 heroku container:push web --app geekplanet-staging --arg BUILD_ENV="staging"
 heroku container:release web --app geekplanet-staging

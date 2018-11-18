3e805b0ca88f4b293af6183b9b64fbea2157feb8
r3dDoX <r3dDoX@users.noreply.github.com>
2018-11-18 12:11:35 +0100

    move cat into before_deploy script and remove explicit docker login

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-HEROKU_API_KEY="$1" heroku container:login
+heroku container:login
 heroku container:push web --app geekplanet --arg BUILD_ENV="production"
+heroku container:release web --app geekplanet

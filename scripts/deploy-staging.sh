1ec4fe1ba99cf30f1bdbd016b583cced53a64142
r3dDoX <patrick@walther-online.ch>
2018-11-18 11:36:30 +0100

    remove heroku login

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 echo "$1" | docker login -u=_ --password-stdin registry.heroku.com
-HEROKU_API_KEY="$1" heroku container:login
 heroku container:push web --app geekplanet-staging --arg BUILD_ENV="staging"
 heroku container:release web --app geekplanet-staging

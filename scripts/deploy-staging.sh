d8e291cd71c21971e7acb60460dec55d9ca2c271
r3dDoX <patrick@walther-online.ch>
2018-11-18 11:50:54 +0100

    log netrc file

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cat ~/.netrc
+
 HEROKU_API_KEY="$1" heroku login
 echo "$1" | docker login -u=_ --password-stdin registry.heroku.com
 heroku container:push web --app geekplanet-staging --arg BUILD_ENV="staging"
